230915b259709e4d207ad51e2bd807734c85086a
DaggerES <gavazquez@gmail.com>
2016-10-08 00:27:50 +0200

    Edit masterserver

--- MasterServer/Program.cs
+++ MasterServer/Program.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace MasterServer
 {
     /// <summary>
     /// This program is the one who does the punchtrough. You should only run if you agree in the forum to do so and your server ip is listed in:
-    /// 
+    /// https://raw.githubusercontent.com/DaggerES/LunaMultiPlayer/master/MasterServersList
     /// </summary>
     class Program
     {
