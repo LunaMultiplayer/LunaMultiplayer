56e262876908c8bfba19c1b184f031a60c9fa219
Gabriel Vazquez <gavazquez@gmail.com>
2016-10-28 14:44:08 +0200

    Update CopyToKSPDirectory.bat

--- CopyToKSPDirectory.bat
+++ CopyToKSPDirectory.bat
@@ -1,3 +1,6 @@
+::THIS FILE IS BEING UNTRACKED FROM GIT with the command: git update-index --assume-unchanged <file>
+::To resume the tracking of changes to this file use the command: git update-index --no-assume-unchanged <file>
+
 ::Set the path below as you need and then run this bat to copy the files and make them debuggeable
 SET KSPPATH=D:\Steam\SteamApps\common\Kerbal Space Program
 
