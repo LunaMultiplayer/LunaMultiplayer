541aea2b001832779db966db5285774f88fad4b9
Lothsahn <Lothsahn@gmail.com>
2016-10-29 16:36:48 -0400

    Updated CopyToKSPDirectory.bat to reference KSPPATH for all copy commands, and to refer to the new folder structure.

--- CopyToKSPDirectory.bat
+++ CopyToKSPDirectory.bat
@@ -2,24 +2,24 @@
 ::To resume the tracking of changes to this file use the command: git update-index --no-assume-unchanged <file>
 
 ::Set the path below as you need and then run this bat to copy the files and make them debuggeable
-SET KSPPATH=D:\Steam\SteamApps\common\Kerbal Space Program
+SET KSPPATH=C:\games\Steam\SteamApps\common\Kerbal Space Program
 
 mkdir "%KSPPATH%\GameData\LunaMultiPlayer\"
 mkdir "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
 
-".\ExternalLibraries\pdb2mdb\pdb2mdb.exe" ".\Client\bin\Debug\LunaClient.dll"
+".\External\pdb2mdb\pdb2mdb.exe" ".\Client\bin\Debug\LunaClient.dll"
 xcopy /Y ".\Client\bin\Debug\LunaClient.dll" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
 xcopy /Y ".\Client\bin\Debug\LunaClient.pdb" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
 xcopy /Y ".\Client\bin\Debug\LunaClient.dll.mdb" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
 
-".\ExternalLibraries\pdb2mdb\pdb2mdb.exe" ".\Common\bin\Debug\LunaCommon.dll"
+".\External\pdb2mdb\pdb2mdb.exe" ".\Common\bin\Debug\LunaCommon.dll"
 xcopy /Y ".\Common\bin\Debug\LunaCommon.dll" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
 xcopy /Y ".\Common\bin\Debug\LunaCommon.pdb" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
 xcopy /Y ".\Common\bin\Debug\LunaCommon.dll.mdb" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
 
-xcopy /Y ".\ExternalLibraries\FastMember.dll" "D:\Steam\SteamApps\common\Kerbal Space Program\GameData\LunaMultiPlayer\Plugins"
-xcopy /Y ".\ExternalLibraries\Lidgren.Network.dll" "D:\Steam\SteamApps\common\Kerbal Space Program\GameData\LunaMultiPlayer\Plugins"
-xcopy /Y ".\ExternalLibraries\System.Threading.dll" "D:\Steam\SteamApps\common\Kerbal Space Program\GameData\LunaMultiPlayer\Plugins"
-xcopy /Y ".\ExternalLibraries\System.Data.dll" "D:\Steam\SteamApps\common\Kerbal Space Program\GameData\LunaMultiPlayer\Plugins"
-xcopy /Y ".\ExternalLibraries\Mono.Data.Tds.dll" "D:\Steam\SteamApps\common\Kerbal Space Program\GameData\LunaMultiPlayer\Plugins"
-xcopy /Y ".\ExternalLibraries\System.Transactions.dll" "D:\Steam\SteamApps\common\Kerbal Space Program\GameData\LunaMultiPlayer\Plugins"
+xcopy /Y ".\External\Dependencies\FastMember.dll" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
+xcopy /Y ".\External\Dependencies\Lidgren.Network.dll" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
+xcopy /Y ".\External\Dependencies\System.Threading.dll" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
+xcopy /Y ".\External\Dependencies\System.Data.dll" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
+xcopy /Y ".\External\Dependencies\Mono.Data.Tds.dll" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
+xcopy /Y ".\External\Dependencies\System.Transactions.dll" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
