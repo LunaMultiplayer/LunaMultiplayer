2d76cf0fe0dd8bd61f167241be81c150705bdac4
DaggerES <gavazquez@gmail.com>
2016-10-30 10:49:55 +0100

    Fix for the execution of the batch after compiling. Closes #4

--- CopyToKSPDirectory.bat
+++ CopyToKSPDirectory.bat
@@ -1,5 +1,6 @@
 ::THIS FILE IS BEING UNTRACKED FROM GIT with the command: git update-index --assume-unchanged <file>
 ::To resume the tracking of changes to this file use the command: git update-index --no-assume-unchanged <file>
+::You must keep this file in the solution folder for it to work
 
 ::Set the path below as you need and then run this bat to copy the files and make them debuggeable
 SET KSPPATH=C:\games\Steam\SteamApps\common\Kerbal Space Program
@@ -7,10 +8,10 @@ SET KSPPATH=C:\games\Steam\SteamApps\common\Kerbal Space Program
 mkdir "%KSPPATH%\GameData\LunaMultiPlayer\"
 mkdir "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
 
-".\External\pdb2mdb\pdb2mdb.exe" ".\Client\bin\Debug\LunaClient.dll"
-xcopy /Y ".\Client\bin\Debug\LunaClient.*" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
+"%~dp0\External\pdb2mdb\pdb2mdb.exe" "%~dp0\Client\bin\Debug\LunaClient.dll"
+xcopy /Y "%~dp0\Client\bin\Debug\LunaClient.*" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
 
-".\External\pdb2mdb\pdb2mdb.exe" ".\Common\bin\Debug\LunaCommon.dll"
-xcopy /Y ".\Common\bin\Debug\LunaCommon.*" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
+"%~dp0\External\pdb2mdb\pdb2mdb.exe" "%~dp0\Common\bin\Debug\LunaCommon.dll"
+xcopy /Y "%~dp0\Common\bin\Debug\LunaCommon.*" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
 
-xcopy /Y ".\External\Dependencies\*.dll" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
+xcopy /Y "%~dp0\External\Dependencies\*.dll" "%KSPPATH%\GameData\LunaMultiPlayer\Plugins"
