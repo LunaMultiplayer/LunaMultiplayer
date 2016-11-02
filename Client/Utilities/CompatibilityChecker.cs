69c780e9af4ad78e8fa33275521def1415666f87
DaggerES <gavazquez@gmail.com>
2016-11-02 10:22:00 +0100

    Update for 1.2.1 and fix disclaimer

--- Client/Utilities/CompatibilityChecker.cs
+++ Client/Utilities/CompatibilityChecker.cs
@@ -53,7 +53,7 @@ namespace LunaClient.Utilities
         {
             const int compatibleMajor = 1;
             const int compatibleMinor = 2;
-            const int compatibleRevision = 0;
+            const int compatibleRevision = 1;
             return (Versioning.version_major == compatibleMajor) && (Versioning.version_minor == compatibleMinor) && (Versioning.Revision == compatibleRevision);
 
             /*-----------------------------------------------*\
--- Client/Windows/Disclaimer/DisclaimerDrawer.cs
+++ Client/Windows/Disclaimer/DisclaimerDrawer.cs
@@ -1,6 +1,4 @@
-﻿using LunaClient.Base.Interface;
-using LunaClient.Systems.SettingsSys;
-using LunaClient.Utilities;
+﻿using LunaClient.Systems.SettingsSys;
 using UnityEngine;
 
 namespace LunaClient.Windows.Disclaimer
@@ -12,13 +10,13 @@ namespace LunaClient.Windows.Disclaimer
             GUILayout.BeginVertical();
             GUI.DragWindow(MoveRect);
 
-            var disclaimerText = "LunaMultiPlayer shares the following personally identifiable information with any server you connect to.\n";
-            disclaimerText += "a) Your player Name you connect with.\n";
+            var disclaimerText = "Luna Multi Player (LMP) shares the following personally identifiable information with the master server and any server you connect to.\n";
+            disclaimerText += "a) Your player name you connect with.\n";
             disclaimerText += "b) Your player token (A randomly generated string to authenticate you).\n";
             disclaimerText += "c) Your IP address is logged on the server console.\n";
             disclaimerText += "\n";
-            disclaimerText += "LMP does not contact any other computer than the server you are connecting to.\n";
-            disclaimerText += "In order to use LunaMultiPlayer, you must allow LMP to use this info\n";
+            disclaimerText += "LMP does not contact any other computer than the server you are connecting to and the master server.\n";
+            disclaimerText += "In order to use LMP, you must allow it to use this info\n";
             disclaimerText += "\n";
             disclaimerText += "For more information - see the KSP addon rules\n";
             GUILayout.Label(disclaimerText);
@@ -26,14 +24,14 @@ namespace LunaClient.Windows.Disclaimer
             if (GUILayout.Button("Open the KSP Addon rules in the browser"))
                 Application.OpenURL(
                     "http://forum.kerbalspaceprogram.com/threads/87841-Add-on-Posting-Rules-July-24th-2014-going-into-effect-August-21st-2014!");
-            if (GUILayout.Button("I accept - Enable LunaMultiPlayer"))
+            if (GUILayout.Button("I accept - Enable LMP"))
             {
                 Display = false;
                 SettingsSystem.CurrentSettings.DisclaimerAccepted = true;
                 MainSystem.Singleton.Enabled = true;
                 SettingsSystem.Singleton.SaveSettings();
             }
-            if (GUILayout.Button("I decline - Disable LunaMultiPlayer"))
+            if (GUILayout.Button("I decline - Disable LMP"))
             {
                 MainSystem.Singleton.ShowGui = false;
                 Display = false;
