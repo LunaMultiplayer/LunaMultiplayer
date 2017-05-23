b9c99b0efb3e0270f0fc44ea2755543cca35a017
Gabriel <gavazquez@gmail.com>
2017-05-23 15:06:48 +0200

    System window improvements

--- Client/Windows/Systems/SystemsDrawer.cs
+++ Client/Windows/Systems/SystemsDrawer.cs
@@ -52,10 +52,22 @@ namespace LunaClient.Windows.Systems
             }
             GUILayout.EndHorizontal();
 
+            ScrollPos = GUILayout.BeginScrollView(ScrollPos, GUILayout.Width(WindowWidth - 5), GUILayout.Height(WindowHeight - 100));
+
+            PrintSystemButtons();
+
+            GUILayout.EndScrollView();
+
+            GUILayout.EndVertical();
+        }
+
+        private void PrintSystemButtons()
+        {
             CraftLibrary = GUILayout.Toggle(CraftLibrary, "Craft library system", ButtonStyle);
             if (CraftLibrary)
             {
-                CraftLibrarySystem.Singleton.Enabled = GUILayout.Toggle(CraftLibrarySystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
+                CraftLibrarySystem.Singleton.Enabled = GUILayout.Toggle(CraftLibrarySystem.Singleton.Enabled, "ON/OFF",
+                    ButtonStyle);
                 if (!string.IsNullOrEmpty(CraftLibraryProfilerText))
                     GUILayout.Label(CraftLibraryProfilerText, LabelStyle);
             }
@@ -114,87 +126,101 @@ namespace LunaClient.Windows.Systems
             VesselSystems = GUILayout.Toggle(VesselSystems, "Vessel systems", ButtonStyle);
             if (VesselSystems)
             {
-                VesselChange = GUILayout.Toggle(VesselChange, "Vessel change", ButtonStyle);
-                if (VesselChange)
-                {
-                    VesselChangeSystem.Singleton.Enabled = GUILayout.Toggle(VesselChangeSystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
-                    if (!string.IsNullOrEmpty(VesselChangeProfilerText))
-                        GUILayout.Label(VesselChangeProfilerText, LabelStyle);
-                }
-
-                VesselDock = GUILayout.Toggle(VesselDock, "Vessel dock", ButtonStyle);
-                if (VesselDock)
-                {
-                    VesselDockSystem.Singleton.Enabled = GUILayout.Toggle(VesselDockSystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
-                    if (!string.IsNullOrEmpty(VesselDockProfilerText))
-                        GUILayout.Label(VesselDockProfilerText, LabelStyle);
-                }
-
-                VesselFlightState = GUILayout.Toggle(VesselFlightState, "Vessel flightstate", ButtonStyle);
-                if (VesselFlightState)
-                {
-                    VesselFlightStateSystem.Singleton.Enabled = GUILayout.Toggle(VesselFlightStateSystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
-                    if (!string.IsNullOrEmpty(VesselFlightStateProfilerText))
-                        GUILayout.Label(VesselFlightStateProfilerText, LabelStyle);
-                }
-
-                VesselImmortal = GUILayout.Toggle(VesselImmortal, "Vessel immortal", ButtonStyle);
-                if (VesselImmortal)
-                {
-                    VesselImmortalSystem.Singleton.Enabled = GUILayout.Toggle(VesselImmortalSystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
-                    if (!string.IsNullOrEmpty(VesselImmortalProfilerText))
-                        GUILayout.Label(VesselImmortalProfilerText, LabelStyle);
-                }
-
-                VesselLock = GUILayout.Toggle(VesselLock, "Vessel lock", ButtonStyle);
-                if (VesselLock)
-                {
-                    VesselLockSystem.Singleton.Enabled = GUILayout.Toggle(VesselLockSystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
-                    if (!string.IsNullOrEmpty(VesselLockProfilerText))
-                        GUILayout.Label(VesselLockProfilerText, LabelStyle);
-                }
-
-                VesselPosition = GUILayout.Toggle(VesselPosition, "Vessel position", ButtonStyle);
-                if (VesselPosition)
-                {
-                    VesselPositionSystem.Singleton.Enabled = GUILayout.Toggle(VesselPositionSystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
-                    if (!string.IsNullOrEmpty(VesselPositionProfilerText))
-                        GUILayout.Label(VesselPositionProfilerText, LabelStyle);
-                }
-
-                VesselProto = GUILayout.Toggle(VesselProto, "Vessel proto", ButtonStyle);
-                if (VesselProto)
-                {
-                    VesselProtoSystem.Singleton.Enabled = GUILayout.Toggle(VesselProtoSystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
-                    if (!string.IsNullOrEmpty(VesselProtoProfilerText))
-                        GUILayout.Label(VesselProtoProfilerText, LabelStyle);
-                }
-
-                VesselRange = GUILayout.Toggle(VesselRange, "Vessel range", ButtonStyle);
-                if (VesselRange)
-                {
-                    VesselRangeSystem.Singleton.Enabled = GUILayout.Toggle(VesselRangeSystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
-                    if (!string.IsNullOrEmpty(VesselRangeProfilerText))
-                        GUILayout.Label(VesselRangeProfilerText, LabelStyle);
-                }
-
-                VesselRemove = GUILayout.Toggle(VesselRemove, "Vessel remove", ButtonStyle);
-                if (VesselRemove)
-                {
-                    VesselRemoveSystem.Singleton.Enabled = GUILayout.Toggle(VesselRemoveSystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
-                    if (!string.IsNullOrEmpty(VesselRemoveProfilerText))
-                        GUILayout.Label(VesselRemoveProfilerText, LabelStyle);
-                }
-
-                VesselUpdate = GUILayout.Toggle(VesselUpdate, "Vessel update", ButtonStyle);
-                if (VesselUpdate)
-                {
-                    VesselUpdateSystem.Singleton.Enabled = GUILayout.Toggle(VesselUpdateSystem.Singleton.Enabled, "ON/OFF", ButtonStyle);
-                    if (!string.IsNullOrEmpty(VesselUpdateProfilerText))
-                        GUILayout.Label(VesselUpdateProfilerText, LabelStyle);
-                }
+                PrintVesselSystems();
             }
-            GUILayout.EndVertical();
+        }
+
+        private void PrintVesselSystems()
+        {
+            VesselChange = GUILayout.Toggle(VesselChange, "Vessel change", ButtonStyle);
+            if (VesselChange)
+            {
+                VesselChangeSystem.Singleton.Enabled = GUILayout.Toggle(VesselChangeSystem.Singleton.Enabled, "ON/OFF",
+                    ButtonStyle);
+                if (!string.IsNullOrEmpty(VesselChangeProfilerText))
+                    GUILayout.Label(VesselChangeProfilerText, LabelStyle);
+            }
+
+            VesselDock = GUILayout.Toggle(VesselDock, "Vessel dock", ButtonStyle);
+            if (VesselDock)
+            {
+                VesselDockSystem.Singleton.Enabled = GUILayout.Toggle(VesselDockSystem.Singleton.Enabled, "ON/OFF",
+                    ButtonStyle);
+                if (!string.IsNullOrEmpty(VesselDockProfilerText))
+                    GUILayout.Label(VesselDockProfilerText, LabelStyle);
+            }
+
+            VesselFlightState = GUILayout.Toggle(VesselFlightState, "Vessel flightstate", ButtonStyle);
+            if (VesselFlightState)
+            {
+                VesselFlightStateSystem.Singleton.Enabled = GUILayout.Toggle(VesselFlightStateSystem.Singleton.Enabled,
+                    "ON/OFF", ButtonStyle);
+                if (!string.IsNullOrEmpty(VesselFlightStateProfilerText))
+                    GUILayout.Label(VesselFlightStateProfilerText, LabelStyle);
+            }
+
+            VesselImmortal = GUILayout.Toggle(VesselImmortal, "Vessel immortal", ButtonStyle);
+            if (VesselImmortal)
+            {
+                VesselImmortalSystem.Singleton.Enabled = GUILayout.Toggle(VesselImmortalSystem.Singleton.Enabled, "ON/OFF",
+                    ButtonStyle);
+                if (!string.IsNullOrEmpty(VesselImmortalProfilerText))
+                    GUILayout.Label(VesselImmortalProfilerText, LabelStyle);
+            }
+
+            VesselLock = GUILayout.Toggle(VesselLock, "Vessel lock", ButtonStyle);
+            if (VesselLock)
+            {
+                VesselLockSystem.Singleton.Enabled = GUILayout.Toggle(VesselLockSystem.Singleton.Enabled, "ON/OFF",
+                    ButtonStyle);
+                if (!string.IsNullOrEmpty(VesselLockProfilerText))
+                    GUILayout.Label(VesselLockProfilerText, LabelStyle);
+            }
+
+            VesselPosition = GUILayout.Toggle(VesselPosition, "Vessel position", ButtonStyle);
+            if (VesselPosition)
+            {
+                VesselPositionSystem.Singleton.Enabled = GUILayout.Toggle(VesselPositionSystem.Singleton.Enabled, "ON/OFF",
+                    ButtonStyle);
+                if (!string.IsNullOrEmpty(VesselPositionProfilerText))
+                    GUILayout.Label(VesselPositionProfilerText, LabelStyle);
+            }
+
+            VesselProto = GUILayout.Toggle(VesselProto, "Vessel proto", ButtonStyle);
+            if (VesselProto)
+            {
+                VesselProtoSystem.Singleton.Enabled = GUILayout.Toggle(VesselProtoSystem.Singleton.Enabled, "ON/OFF",
+                    ButtonStyle);
+                if (!string.IsNullOrEmpty(VesselProtoProfilerText))
+                    GUILayout.Label(VesselProtoProfilerText, LabelStyle);
+            }
+
+            VesselRange = GUILayout.Toggle(VesselRange, "Vessel range", ButtonStyle);
+            if (VesselRange)
+            {
+                VesselRangeSystem.Singleton.Enabled = GUILayout.Toggle(VesselRangeSystem.Singleton.Enabled, "ON/OFF",
+                    ButtonStyle);
+                if (!string.IsNullOrEmpty(VesselRangeProfilerText))
+                    GUILayout.Label(VesselRangeProfilerText, LabelStyle);
+            }
+
+            VesselRemove = GUILayout.Toggle(VesselRemove, "Vessel remove", ButtonStyle);
+            if (VesselRemove)
+            {
+                VesselRemoveSystem.Singleton.Enabled = GUILayout.Toggle(VesselRemoveSystem.Singleton.Enabled, "ON/OFF",
+                    ButtonStyle);
+                if (!string.IsNullOrEmpty(VesselRemoveProfilerText))
+                    GUILayout.Label(VesselRemoveProfilerText, LabelStyle);
+            }
+
+            VesselUpdate = GUILayout.Toggle(VesselUpdate, "Vessel update", ButtonStyle);
+            if (VesselUpdate)
+            {
+                VesselUpdateSystem.Singleton.Enabled = GUILayout.Toggle(VesselUpdateSystem.Singleton.Enabled, "ON/OFF",
+                    ButtonStyle);
+                if (!string.IsNullOrEmpty(VesselUpdateProfilerText))
+                    GUILayout.Label(VesselUpdateProfilerText, LabelStyle);
+            }
         }
     }
 }
