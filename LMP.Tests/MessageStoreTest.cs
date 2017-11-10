d7d610ad956a5e839676d93d6f230245ad652cd9
Gabriel Vazquez <gavazquez@gmail.com>
2017-11-10 22:36:41 +0100

    Update MessageStoreTest.cs

--- LMP.Tests/MessageStoreTest.cs
+++ LMP.Tests/MessageStoreTest.cs
@@ -15,10 +15,12 @@ namespace LMP.Tests
         public void TestMsgMessageStore()
         {
             var msg1 = Factory.CreateNew<VesselSrvMsg, VesselPositionMsgData>();
+            
             Assert.AreEqual(0, MessageStore.GetMessageCount(typeof(VesselSrvMsg)));
             Assert.AreEqual(0, MessageStore.GetMessageDataCount(typeof(VesselPositionMsgData)));
 
             var msg2 = Factory.CreateNew<VesselSrvMsg, VesselPositionMsgData>();
+            
             Assert.AreEqual(0, MessageStore.GetMessageCount(typeof(VesselSrvMsg)));
             Assert.AreEqual(0, MessageStore.GetMessageDataCount(typeof(VesselPositionMsgData)));
 
@@ -27,15 +29,20 @@ namespace LMP.Tests
 
             Assert.AreEqual(1, MessageStore.GetMessageCount(typeof(VesselSrvMsg)));
             Assert.AreEqual(1, MessageStore.GetMessageDataCount(typeof(VesselPositionMsgData)));
+            
             //If we retrieve a new message the first one should be reused
             var msg3 = Factory.CreateNew<VesselSrvMsg, VesselPositionMsgData>();
             
             msg2.Recycle();
             msg3.Recycle();
 
+            Assert.AreEqual(2, MessageStore.GetMessageCount(typeof(VesselSrvMsg)));
+            Assert.AreEqual(2, MessageStore.GetMessageDataCount(typeof(VesselPositionMsgData)));
+
             var msg4 = Factory.CreateNew<VesselSrvMsg, VesselPositionMsgData>();
-            Assert.AreEqual(3, MessageStore.GetMessageCount(typeof(VesselSrvMsg)));
-            Assert.AreEqual(3, MessageStore.GetMessageDataCount(typeof(VesselPositionMsgData)));
+            
+            Assert.AreEqual(1, MessageStore.GetMessageCount(typeof(VesselSrvMsg)));
+            Assert.AreEqual(1, MessageStore.GetMessageDataCount(typeof(VesselPositionMsgData)));
         }
     }
 }
