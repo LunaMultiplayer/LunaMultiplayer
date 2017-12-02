bf4d5a6f1020d77a69569d3d05dd584c96f93a52
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2017-12-02 18:12:30 +0100

    New version 0.0.2

--- LMP.Tests/Properties/AssemblyInfo.cs
+++ LMP.Tests/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("51f95d0b-c12d-44a2-b390-f29e9491ff8f")]
 
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.1-compiled")]
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
+[assembly: AssemblyInformationalVersion("0.0.2-compiled")]
