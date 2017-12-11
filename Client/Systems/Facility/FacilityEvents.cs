23beaaaec0fe5f706daefded3d12ece6b708c891
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2017-12-11 11:10:33 +0100

    Fixed the spamming of facilities repair /destroy

--- Client/Systems/Facility/FacilityEvents.cs
+++ Client/Systems/Facility/FacilityEvents.cs
@@ -1,4 +1,5 @@
 ﻿using LunaClient.Base;
+using LunaClient.Systems.Scenario;
 using LunaClient.VesselUtilities;
 using Upgradeables;
 
@@ -9,18 +10,22 @@ namespace LunaClient.Systems.Facility
         public void FacilityUpgraded(UpgradeableFacility building, int level)
         {
             System.MessageSender.SendFacilityUpgradeMsg(building.id, level);
+            SystemsContainer.Get<ScenarioSystem>().SendScenarioModules();
         }
 
         public void FacilityRepaired(DestructibleBuilding building)
         {
             System.MessageSender.SendFacilityRepairMsg(building.id);
+            SystemsContainer.Get<ScenarioSystem>().SendScenarioModules();
         }
 
         public void FacilityCollapsing(DestructibleBuilding building)
         {
+            //Don't send this kind of messages when spectating as they are not accurate
             if (!VesselCommon.IsSpectating)
             {
                 System.MessageSender.SendFacilityCollapseMsg(building.id);
+                SystemsContainer.Get<ScenarioSystem>().SendScenarioModules();
             }
         }
     }
