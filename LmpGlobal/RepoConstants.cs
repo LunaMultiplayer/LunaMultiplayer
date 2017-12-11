3fd3b06a27a8d928b7e8dfc7998a7423eb05e904
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2017-12-11 18:13:26 +0100

    Fix url

--- LmpGlobal/RepoConstants.cs
+++ LmpGlobal/RepoConstants.cs
@@ -3,7 +3,7 @@
     public static class RepoConstants
     {
         public static string RepoUrl => "http://github.com/gavazquez/LunaMultiPlayer/releases/latest";
-        public static string MasterServersListShortUrl => "https://goo.gl/NJqZbc";
+        public static string MasterServersListShortUrl => "https://goo.gl/6B6TWZ";
         public static string MasterServersListUrl => "http://raw.githubusercontent.com/gavazquez/LunaMultiPlayer/master/MasterServersList/MasterServersList.txt";
         public static string LatestReleaseUrl => "http://api.github.com/repos/gavazquez/LunaMultiPlayer/releases/latest";
     }
