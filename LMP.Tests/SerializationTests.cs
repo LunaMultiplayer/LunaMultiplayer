97d97da253011df80d7b2bc279ed0bd6d140f0ee
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-01-19 13:51:08 +0100

    Added one more test

--- LMP.Tests/SerializationTests.cs
+++ LMP.Tests/SerializationTests.cs
@@ -1,5 +1,6 @@
 ﻿using Lidgren.Network;
 using LunaCommon.Message;
+using LunaCommon.Message.Client;
 using LunaCommon.Message.Data.Vessel;
 using LunaCommon.Message.Server;
 using LunaCommon.Time;
@@ -13,11 +14,12 @@ namespace LMP.Tests
     public class SerializationTests
     {
         private static readonly ServerMessageFactory Factory = new ServerMessageFactory();
+        private static readonly Random Rnd = new Random();
+        private static readonly NetClient Client = new NetClient(new NetPeerConfiguration("TESTS"));
 
         [TestMethod]
         public void TestSerializeDeserialize()
         {
-            var client = new NetClient(new NetPeerConfiguration("TESTS"));
             var bytes = new byte[10000];
             new Random().NextBytes(bytes);
 
@@ -31,7 +33,7 @@ namespace LMP.Tests
 
             //Serialize and compress
             var expectedDataSize = msg.GetMessageSize(false);
-            var lidgrenMsgSend = client.CreateMessage(expectedDataSize);
+            var lidgrenMsgSend = Client.CreateMessage(expectedDataSize);
             msg.Serialize(lidgrenMsgSend, false);
             var realSize = lidgrenMsgSend.LengthBytes;
 
@@ -40,7 +42,7 @@ namespace LMP.Tests
 
             //Simulate sending
             var data = lidgrenMsgSend.ReadBytes(lidgrenMsgSend.LengthBytes);
-            var lidgrenMsgRecv = client.CreateIncomingMessage(NetIncomingMessageType.Data, data);
+            var lidgrenMsgRecv = Client.CreateIncomingMessage(NetIncomingMessageType.Data, data);
             lidgrenMsgRecv.LengthBytes = lidgrenMsgSend.LengthBytes;
 
             //Deserialize
@@ -51,6 +53,58 @@ namespace LMP.Tests
             Array.Resize(ref ((VesselProtoMsgData)msgDes.Data).Vessel.Data, 10000);
 
             Assert.IsTrue(((VesselProtoMsgData)msg.Data).Vessel.Data.SequenceEqual(((VesselProtoMsgData)msgDes.Data).Vessel.Data));
+        }
+
+        [TestMethod]
+        public void TestSerializeDeserializeVesselUpdateMsg()
+        {
+            var msgData = Factory.CreateNewMessageData<VesselUpdateMsgData>();
+            msgData.VesselId = Guid.NewGuid();
+            msgData.Name = "Name";
+            msgData.Type = "Type";
+            msgData.Situation = "Situation";
+            msgData.Landed = true;
+            msgData.LandedAt = "LandedAt";
+            msgData.DisplayLandedAt = "DisplayLandedAt";
+            msgData.Splashed = false;
+            msgData.MissionTime = Rnd.NextDouble();
+            msgData.LaunchTime = Rnd.NextDouble();
+            msgData.LastUt = Rnd.NextDouble();
+            msgData.Persistent = false;
+            msgData.RefTransformId = (uint)Rnd.Next();
+            msgData.Controllable = true;
+
+            for (var i = 0; i < 17; i++)
+            {
+                if (msgData.ActionGroups[i] == null)
+                    msgData.ActionGroups[i] = new ActionGroup();
+
+                msgData.ActionGroups[i].ActionGroupName = "ActionGroupName" + i;
+                msgData.ActionGroups[i].State = true;
+                msgData.ActionGroups[i].Time = Rnd.NextDouble();
+            }
+
+            var msg = Factory.CreateNew<VesselCliMsg>(msgData);
+            
+            //Serialize and compress
+            var expectedDataSize = msg.GetMessageSize(false);
+            var lidgrenMsgSend = Client.CreateMessage(expectedDataSize);
+            msg.Serialize(lidgrenMsgSend, false);
+            var realSize = lidgrenMsgSend.LengthBytes;
+
+            //Usually the expected size will be a bit more as Lidgren writes the size of the strings in a base128 int (so it uses less bytes)
+            Assert.IsTrue(expectedDataSize >= realSize);
+
+            //Simulate sending
+            var data = lidgrenMsgSend.ReadBytes(lidgrenMsgSend.LengthBytes);
+            var lidgrenMsgRecv = Client.CreateIncomingMessage(NetIncomingMessageType.Data, data);
+            lidgrenMsgRecv.LengthBytes = lidgrenMsgSend.LengthBytes;
+
+            msg.Recycle();
+
+            //Deserialize
+            var msgDes = Factory.Deserialize(lidgrenMsgRecv, Environment.TickCount);
+
         }
     }
 }
