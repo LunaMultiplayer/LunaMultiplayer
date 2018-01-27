22a3bd43028950977e9ca53dc23a90d9c8a71fd9
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-01-27 15:36:35 +0100

    New repo constants urls

--- LmpGlobal/RepoConstants.cs
+++ LmpGlobal/RepoConstants.cs
@@ -2,9 +2,9 @@
 {
     public static class RepoConstants
     {
-        public static string RepoUrl => "http://github.com/gavazquez/LunaMultiPlayer/releases/latest";
-        public static string MasterServersListShortUrl => "https://goo.gl/6B6TWZ";
-        public static string MasterServersListUrl => "http://raw.githubusercontent.com/gavazquez/LunaMultiPlayer/master/MasterServersList/MasterServersList.txt";
-        public static string LatestReleaseUrl => "http://api.github.com/repos/gavazquez/LunaMultiPlayer/releases/latest";
+        public static string RepoUrl => "http://github.com/LunaMultiplayer/LunaMultiplayer/releases/latest";
+        public static string MasterServersListShortUrl => "https://goo.gl/jgUgU9";
+        public static string MasterServersListUrl => "http://raw.githubusercontent.com/LunaMultiplayer/LunaMultiplayer/master/MasterServersList/MasterServersList.txt";
+        public static string LatestReleaseUrl => "http://api.github.com/repos/LunaMultiplayer/LunaMultiplayer/releases/latest";
     }
 }
