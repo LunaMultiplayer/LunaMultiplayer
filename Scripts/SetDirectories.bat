61671192ea7404cfa3cb240a6ad07476a6462773
Lothsahn <Lothsahn@gmail.com>
2018-02-09 19:02:33 -0500

    Check in the directories that I need to use.

---
+++ Scripts/SetDirectories.bat
@@ -0,0 +1,7 @@
+::THIS FILE IS BEING UNTRACKED FROM GIT with the command: git update-index --skip-worktree <file>
+::To resume the tracking of changes to this file use the command: git update-index --no-skip-worktree <file>
+
+::Set the path below as you need and then run this bat to copy the files and make them debuggeable
+SET KSPPATH=C:\work\builds\Kerbal Space Program
+::Set another path in case you run two KSP instances otherwise leave empty
+SET KSPPATH2=C:\work\builds\Kerbal Space Program2
