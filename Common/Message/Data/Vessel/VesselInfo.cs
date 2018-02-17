e700e5f0fa405edbd0f42ab6feb59688517bc252
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-02-17 12:51:15 +0100

    Removed compression from vessel proto

--- Common/Message/Data/Vessel/VesselInfo.cs
+++ Common/Message/Data/Vessel/VesselInfo.cs
@@ -1,5 +1,4 @@
 ﻿using Lidgren.Network;
-using LunaCommon.Compression;
 using LunaCommon.Message.Base;
 using System;
 
@@ -16,9 +15,13 @@ namespace LunaCommon.Message.Data.Vessel
         {
             GuidUtil.Serialize(VesselId, lidgrenMsg);
 
-            Array.Resize(ref Data, NumBytes);
-            Data = CompressionHelper.compress(Data, 3);
-            NumBytes = Data.Length;
+            //TODO: This creates an issue!! The problem is that the server reads this message (so it deserializes it) 
+            //and then it directly relays it so it's not properly compressed. Then on the client it tries to decompress it and the message is corrupted.
+            //As a result the message becomes corrupt
+
+            //Array.Resize(ref Data, NumBytes);
+            //Data = CompressionHelper.compress(Data, 3);
+            //NumBytes = Data.Length;
 
             lidgrenMsg.Write(NumBytes);
             lidgrenMsg.Write(Data, 0, NumBytes);
@@ -34,9 +37,9 @@ namespace LunaCommon.Message.Data.Vessel
 
             lidgrenMsg.ReadBytes(Data, 0, NumBytes);
 
-            Array.Resize(ref Data, NumBytes);
-            Data = CompressionHelper.decompress(Data);
-            NumBytes = Data.Length;
+            //Array.Resize(ref Data, NumBytes);
+            //Data = CompressionHelper.decompress(Data);
+            //NumBytes = Data.Length;
         }
 
         public int GetByteCount()
