caee5dbfae5cac9858f39514cf59c0ae1a0de016
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-03-14 12:43:42 +0100

    Update new version + update litedb

--- LunaUpdater/Properties/AssemblyInfo.cs
+++ LunaUpdater/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("35a57b77-236e-492c-b003-1f8fc2f544f3")]
 
-[assembly: AssemblyVersion("0.3.26")]
-[assembly: AssemblyFileVersion("0.3.26")]
-[assembly: AssemblyInformationalVersion("0.3.26-compiled")]
+[assembly: AssemblyVersion("0.3.27")]
+[assembly: AssemblyFileVersion("0.3.27")]
+[assembly: AssemblyInformationalVersion("0.3.27-compiled")]
--- MasterServer/Properties/AssemblyInfo.cs
+++ MasterServer/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("776706b0-f5a1-43a5-b13a-1ada5e77d0a5")]
 
-[assembly: AssemblyVersion("0.3.26")]
-[assembly: AssemblyFileVersion("0.3.26")]
-[assembly: AssemblyInformationalVersion("0.3.26-compiled")]
+[assembly: AssemblyVersion("0.3.27")]
+[assembly: AssemblyFileVersion("0.3.27")]
+[assembly: AssemblyInformationalVersion("0.3.27-compiled")]
