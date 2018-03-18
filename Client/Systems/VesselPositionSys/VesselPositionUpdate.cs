9df3d69f1f522a033ec8504f2edc19948c8a2025
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-03-18 12:38:27 +0100

    Added dark positioning suggestion

--- Client/Systems/VesselPositionSys/VesselPositionUpdate.cs
+++ Client/Systems/VesselPositionSys/VesselPositionUpdate.cs
@@ -195,6 +195,9 @@ namespace LunaClient.Systems.VesselPositionSys
             Vessel.checkLanded();
             Vessel.checkSplashed();
 
+            //TODO: Check if this doesn't break anything
+            Vessel.orbitDriver.orbit.UpdateFromOrbitAtUT(Vessel.orbitDriver.orbit, Planetarium.GetUniversalTime(), Body);
+
             //Set the position of the vessel based on the orbital parameters
             Vessel.orbitDriver.updateFromParameters();
 
