3b62f0daa3d391bb8a45b50c34a88c14f19012e4
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-03-22 14:20:17 +0100

    Better message resource handling

--- Client/Systems/VesselResourceSys/VesselResourceMessageSender.cs
+++ Client/Systems/VesselResourceSys/VesselResourceMessageSender.cs
@@ -5,13 +5,13 @@ using LunaClient.VesselStore;
 using LunaCommon.Message.Client;
 using LunaCommon.Message.Data.Vessel;
 using LunaCommon.Message.Interface;
-using System;
+using System.Collections.Generic;
 
 namespace LunaClient.Systems.VesselResourceSys
 {
     public class VesselResourceMessageSender : SubSystem<VesselResourceSystem>, IMessageSender
     {
-        private static VesselResourceInfo[] _resources = new VesselResourceInfo[0];
+        private static List<VesselResourceInfo> _resources = new List<VesselResourceInfo>();
 
         public void SendMessage(IMessageData msg)
         {
@@ -20,19 +20,11 @@ namespace LunaClient.Systems.VesselResourceSys
 
         public void SendVesselResources(Vessel vessel)
         {
+            _resources.Clear();
+
             var msgData = NetworkMain.CliMsgFactory.CreateNewMessageData<VesselResourceMsgData>();
             msgData.VesselId = vessel.id;
-
-            var resourcesCount = GetResourcesCount(vessel);
-            if (resourcesCount == 0) return; //No need to send msg if there's no resources in the vessel
-
-            msgData.ResourcesCount = resourcesCount;
-
-            if (_resources.Length < resourcesCount)
-                _resources = new VesselResourceInfo[resourcesCount];
-
-            var resourceIndex = 0;
-
+            
             for (var i = 0; i < vessel.protoVessel.protoPartSnapshots.Count; i++)
             {
                 for (var j = 0; j < vessel.protoVessel.protoPartSnapshots[i].resources.Count; j++)
@@ -42,46 +34,28 @@ namespace LunaClient.Systems.VesselResourceSys
 
                     if(resource == null) continue;
 
-                    if (_resources[resourceIndex] == null)
-                        _resources[resourceIndex] = new VesselResourceInfo();
-
-                    _resources[resourceIndex].ResourceName = resource.resourceName;
-                    _resources[resourceIndex].PartFlightId = vessel.protoVessel.protoPartSnapshots[i].flightID;
-
-                    _resources[resourceIndex].Amount = resource.amount;
-                    _resources[resourceIndex].FlowState = resource.flowState;
+                    var resourceInfo = new VesselResourceInfo
+                    {
+                        ResourceName = resource.resourceName,
+                        PartFlightId = vessel.protoVessel.protoPartSnapshots[i].flightID,
+                        Amount = resource.amount,
+                        FlowState = resource.flowState
+                    };
 
                     //Now update the proto values our vessel...
                     protoResource.amount = resource.amount;
                     protoResource.flowState = resource.flowState;
 
-                    resourceIndex++;
+                    _resources.Add(resourceInfo);
                 }
             }
-
-            if (msgData.Resources.Length < resourcesCount)
-                msgData.Resources = new VesselResourceInfo[resourcesCount];
-
-            Array.Copy(_resources, msgData.Resources, resourcesCount);
+            
+            msgData.Resources = _resources.ToArray();
 
             //Update our own values in the store aswell as otherwise if we leave the vessel it can still be in the safety bubble
             VesselsProtoStore.UpdateVesselProtoResources(msgData);
 
             SendMessage(msgData);
-        }
-
-        /// <summary>
-        /// Returns the number of resource in all the parts of the vessel without generating garbage
-        /// </summary>
-        private static int GetResourcesCount(Vessel vessel)
-        {
-            var count = 0;
-            for (var i = 0; i < vessel.Parts.Count; i++)
-            {
-                count += vessel.Parts[i].Resources.Count;
-            }
-
-            return count;
         }
     }
 }
