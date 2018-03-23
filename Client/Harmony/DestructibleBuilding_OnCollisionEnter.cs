15e9429390a9d7e646b34b947cd8f050e52503dd
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-03-23 14:06:01 +0100

    Added a code to skip facility destruction if the colliding vessel is not controlled by you

---
+++ Client/Harmony/DestructibleBuilding_OnCollisionEnter.cs
@@ -0,0 +1,33 @@
+﻿using Harmony;
+using LunaClient.Systems.Lock;
+using LunaClient.Systems.SettingsSys;
+using LunaCommon.Enums;
+using UnityEngine;
+
+// ReSharper disable All
+
+namespace LunaClient.Harmony
+{
+    /// <summary>
+    /// This harmony patch is intended to skip the destruction of a building 
+    /// if the vessel that crashes against it is controlled/updated by another player
+    /// </summary>
+    [HarmonyPatch(typeof(DestructibleBuilding))]
+    [HarmonyPatch("OnCollisionEnter")]
+    public class DestructibleBuilding_OnCollisionEnter
+    {
+        [HarmonyPrefix]
+        private static bool PrefixOnCollisionEnter(Collision c)
+        {
+            if (MainSystem.NetworkState < ClientState.Connected) return true;
+
+            var crashingVessel = c.gameObject.GetComponentUpwards<Part>()?.vessel;
+            if (crashingVessel != null)
+            {
+                return LockSystem.LockQuery.UpdateLockBelongsToPlayer(crashingVessel.id, SettingsSystem.CurrentSettings.PlayerName);
+            }
+
+            return true;
+        }
+    }
+}
