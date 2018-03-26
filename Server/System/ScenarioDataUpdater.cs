ebc590544cde5747a21042a1350ac0b955fa7ac6
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-03-26 14:04:21 +0200

    Added funds scenario patching

--- Server/System/ScenarioDataUpdater.cs
+++ Server/System/ScenarioDataUpdater.cs
@@ -1,6 +1,9 @@
 ﻿using LunaCommon.Xml;
+using Server.Utilities;
 using System.Collections.Concurrent;
+using System.Globalization;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Server.System
 {
@@ -29,5 +32,41 @@ namespace Server.System
                 }
             });
         }
+
+        #region Funds
+
+        /// <summary>
+        /// We received a funds message so update the scenario file accordingly
+        /// </summary>
+        public static void WriteFundsDataToFile(double funds)
+        {
+            Task.Run(() =>
+            {
+                lock (Semaphore.GetOrAdd("Funding", new object()))
+                {
+                    if (!ScenarioStoreSystem.CurrentScenariosInXmlFormat.TryGetValue("Funding", out var xmlData)) return;
+
+                    var updatedText = UpdateScenarioWithFundsData(xmlData, funds);
+                    ScenarioStoreSystem.CurrentScenariosInXmlFormat.TryUpdate("Funding", updatedText, xmlData);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Patches the scenario file with funds data
+        /// </summary>
+        /// <returns></returns>
+        private static string UpdateScenarioWithFundsData(string scenarioData, double funds)
+        {
+            var document = new XmlDocument();
+            document.LoadXml(scenarioData);
+
+            var node = document.SelectSingleNode($"/{ConfigNodeXmlParser.StartElement}/{ConfigNodeXmlParser.ValueNode}[@name='funds']");
+            if (node != null) node.InnerText = funds.ToString(CultureInfo.InvariantCulture);
+
+            return document.ToIndentedString();
+        }
+
+        #endregion
     }
 }
