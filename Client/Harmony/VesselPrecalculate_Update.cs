ce669df732d7a160351b6e5acbbc845241f6f98d
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-03-28 12:06:52 +0200

    Added more code to override the FI

---
+++ Client/Harmony/VesselPrecalculate_Update.cs
@@ -0,0 +1,26 @@
+﻿using Harmony;
+using LunaClient.Systems.Lock;
+using LunaClient.Systems.SettingsSys;
+using LunaCommon.Enums;
+// ReSharper disable All
+
+namespace LunaClient.Harmony
+{
+    [HarmonyPatch(typeof(VesselPrecalculate))]
+    [HarmonyPatch("Update")]
+    public class VesselPrecalculate_Update
+    {
+        [HarmonyPrefix]
+        private static bool PrefixUpdate(VesselPrecalculate __instance)
+        {
+            if (MainSystem.NetworkState < ClientState.Connected) return true;
+
+            if (SettingsSystem.CurrentSettings.OverrideIntegrator && __instance?.Vessel != null)
+            {
+                return LockSystem.LockQuery.UpdateLockBelongsToPlayer(__instance.Vessel.id, SettingsSystem.CurrentSettings.PlayerName);
+            }
+
+            return true;
+        }
+    }
+}
