fe6751c1a42c9bbb05d9511203e521eca94aa914
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-03-30 11:44:58 +0200

    Cleanup

--- Common/Message/Data/ShareProgress/StrategyInfo.cs
+++ Common/Message/Data/ShareProgress/StrategyInfo.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Lidgren.Network;
+﻿using Lidgren.Network;
 using LunaCommon.Message.Base;
+using System;
 
 namespace LunaCommon.Message.Data.ShareProgress
 {
