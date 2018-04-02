d6d0d3dffdf209e4d91fdc9c5e3c65ee35d0462c
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-04-02 11:55:15 +0200

    New version number for development

--- Lidgren.Network/Properties/AssemblyInfo.cs
+++ Lidgren.Network/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("78f2a988-46e1-4fc4-b7b4-dd2cbe46da6a")]
 
-[assembly: AssemblyVersion("0.5.36")]
-[assembly: AssemblyFileVersion("0.5.36")]
-[assembly: AssemblyInformationalVersion("0.5.36-compiled")]
+[assembly: AssemblyVersion("0.5.37")]
+[assembly: AssemblyFileVersion("0.5.37")]
+[assembly: AssemblyInformationalVersion("0.5.37-compiled")]
 
 [assembly: System.CLSCompliant(true)]
 [assembly: InternalsVisibleTo("LMP.Tests")]
--- Server/Properties/AssemblyInfo.cs
+++ Server/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("fa6e9184-e243-49cc-94fa-ac557493b900")]
 
-[assembly: AssemblyVersion("0.5.36")]
-[assembly: AssemblyFileVersion("0.5.36")]
-[assembly: AssemblyInformationalVersion("0.5.36-compiled")]
+[assembly: AssemblyVersion("0.5.37")]
+[assembly: AssemblyFileVersion("0.5.37")]
+[assembly: AssemblyInformationalVersion("0.5.37-compiled")]
