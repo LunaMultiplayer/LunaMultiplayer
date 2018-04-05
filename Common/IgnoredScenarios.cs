4ee0568d0c9df78b28155b9e90c550c531a26557
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-04-05 12:55:25 +0200

    Do not send scenarios data on those systems as they are handled by themselves

--- Common/IgnoredScenarios.cs
+++ Common/IgnoredScenarios.cs
@@ -9,6 +9,19 @@ namespace LunaCommon
         /// Asteorids are handled in a different way so ignore them
         /// CustomWaypoints are useless and not shared between clients
         /// </summary>
-        public static List<string> Names = new List<string> { "ScenarioNewGameIntro", "ScenarioDiscoverableObjects", "ScenarioCustomWaypoints" };
+        public static List<string> Names = new List<string>
+        {
+            "ScenarioNewGameIntro",
+            "ScenarioDiscoverableObjects",
+            "ScenarioCustomWaypoints",
+            "ContractSystem", //This scenario has his own handling system
+            "Funding",//This scenario has his own handling system
+            "ProgressTracking",//This scenario has his own handling system
+            "Reputation",//This scenario has his own handling system
+            "ResearchAndDevelopment",//This scenario has his own handling system
+            "ScenarioDestructibles",//This scenario has his own handling system
+            "ScenarioUpgradeableFacilities",//This scenario has his own handling system
+            "StrategySystem"//This scenario has his own handling system
+        };
     }
 }
