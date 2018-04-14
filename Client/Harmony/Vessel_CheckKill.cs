8d036001d48f5aae62b790a0203626533d725fce
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-04-14 11:11:17 +0200

    Replaced precalc for a harmony patch. Also added some harmony patches for collision checks

---
+++ Client/Harmony/Vessel_CheckKill.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using LunaClient.Systems.Lock;
+using LunaClient.Systems.SettingsSys;
+using LunaCommon.Enums;
+// ReSharper disable All
+
+namespace LunaClient.Harmony
+{
+    /// <summary>
+    /// This harmony patch is intended to avoid run the kill checks in vessels that are not yours
+    /// </summary>
+    [HarmonyPatch(typeof(Vessel))]
+    [HarmonyPatch("CheckKill")]
+    public class Vessel_CheckKill
+    {
+        [HarmonyPrefix]
+        private static bool PrefixCheckKill(Vessel __instance)
+        {
+            if (MainSystem.NetworkState < ClientState.Connected) return true;
+
+            return LockSystem.LockQuery.UnloadedUpdateLockBelongsToPlayer(__instance.id, SettingsSystem.CurrentSettings.PlayerName);
+        }
+    }
+}
