ec6551a28fd39e91512bd01da513a7404c45b496
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-04-24 09:47:20 +0200

    Code cleanup for the change settings command

--- Server/Command/Command/ChangeSettingsCommand.cs
+++ Server/Command/Command/ChangeSettingsCommand.cs
@@ -1,177 +1,128 @@
 ﻿using Server.Command.Command.Base;
 using Server.Command.Common;
+using Server.Context;
 using Server.Log;
 using System;
 using System.IO;
-using System.Reflection;
 using System.Xml;
 
 namespace Server.Command.Command
 {
     public class ChangeSettingsCommand : SimpleCommand
     {
-        //Executes the ChangeSettingsCommand
+        private static string Usage = "Usage: /changesettings settingsFile settingName newValue" +
+                                     $"Description: Parameter settingsFile can be settings, gameplaysettings or debugsettings" +
+                                     $"{Environment.NewLine}Parameter settingName corresponds to the names inside the settings xml files." +
+                                     $"{Environment.NewLine}Parameter settingName is case sensitive." +
+                                     $"{Environment.NewLine}Parameter newValue is the new value." +
+                                     $"{Environment.NewLine}Use \'-\' to set the new value as empty." +
+                                     $"{Environment.NewLine} Examples: This will change server password to empty ..." +
+                                     $"{Environment.NewLine}/changesettings settings Password -" +
+                                     $"{Environment.NewLine}This will change MaxPlayers to 8 ..." +
+                                     $"{Environment.NewLine}/changesettings settings MaxPlayers 8";
+
         public override bool Execute(string commandArgs)
         {
-            //Set messages
-            var msgUsage = Environment.NewLine
-                + Environment.NewLine + "Usage:"
-                + Environment.NewLine + "/changesettings settingsFile settingName newValue";
-
-            var msgSettingFileInfo = Environment.NewLine + "Parameter settingsFile can be settings, gameplaysettings or debugsettings.";
-
-            var msgDescription = Environment.NewLine
-                + Environment.NewLine + "Description:"
-                + msgSettingFileInfo
-                + Environment.NewLine + "Parameter settingName corresponds to the names inside the settings xml files."
-                + Environment.NewLine + "Parameter settingName is case sensitive."
-                + Environment.NewLine + "Parameter newValue is the new value."
-                + Environment.NewLine + "Use '-' to set the new value as empty."
-                + Environment.NewLine + Environment.NewLine + "Examples:"
-                + Environment.NewLine + "This will change server password to empty ..."
-                + Environment.NewLine + "/changesettings settings Password -"
-                + Environment.NewLine + "This will change MaxPlayers to 8 ..."
-                + Environment.NewLine + "/changesettings settings MaxPlayers 8";
-
-            //Get param array from command
             CommandSystemHelperMethods.SplitCommandParamArray(commandArgs, out var parameters);
-            
-            //Check parameters
-            if (parameters == null)
-            {
-                LunaLog.Error(Environment.NewLine + "Syntax error. No parameters found." + msgUsage + msgDescription);
-                return false;
-            }
-            if (parameters.Length != 3)
-            {
-                LunaLog.Error(Environment.NewLine + "Syntax error. Wrong number of parameters." + msgUsage + msgDescription);
-                return false;
-            }
-            if (parameters[0] == "")
-            {
-                LunaLog.Error(Environment.NewLine + "Syntax error. First parameter not found." + msgUsage + msgDescription);
-                return false;
-            }
-            if (parameters[1] == "")
-            {
-                LunaLog.Error(Environment.NewLine + "Syntax error. Second parameter not found." + msgUsage + msgDescription);
-                return false;
-            }
-            if (parameters[2] == "")
-            {
-                LunaLog.Error(Environment.NewLine + "Syntax error. Third parameter not found." + msgUsage + msgDescription);
-                return false;
-            }
 
-            //Get param values
+            if (!ValidateParameters(parameters))
+                return false;
+
             var settingsFile = parameters[0];
-            var settingsFileName = "Settings.xml";
             var settingName = parameters[1];
             var newValue = parameters[2];
 
-            //Check setting file
-            var settingsFileLowered = settingsFile.ToLower();
-            if (settingsFileLowered == "settings")
+            var settingsFileName = GetFileName(settingsFile);
+            if(string.IsNullOrEmpty(settingsFileName))
             {
-                settingsFileName = "Settings.xml";
-            }
-            else if (settingsFileLowered == "gameplaysettings")
-            {
-                settingsFileName = "GameplaySettings.xml";
-            }
-            else if (settingsFileLowered == "debugsettings")
-            {
-                settingsFileName = "DebugSettings.xml";
-            }
-            else
-            {
-                LunaLog.Error(Environment.NewLine + "Syntax error. First parameter (settingsFile) wrong use." + Environment.NewLine + msgSettingFileInfo + msgUsage + msgDescription);
+                LunaLog.Error("Syntax error. First parameter (settingsFile) wrong use");
                 return false;
             }
 
-            //Define settings xml path
-            var settingsPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Config\\" + settingsFileName;
-
-            //Define stream reader and xml document
-            var streamReader = new StreamReader(settingsPath);
-            var document = new XmlDocument();
-
-            //Try to change the settings
+            var settingsPath = Path.Combine(ServerContext.ConfigDirectory, settingsFileName);
             try
             {
-                //Change server settings
-                ChangeServerSettings(settingName, newValue, streamReader, document, settingsPath);
+                ChangeServerSettings(settingName, newValue, settingsPath);
             }
             catch (Exception e)
             {
-                LunaLog.Error(Environment.NewLine + "Exception error: " + Environment.NewLine + e.ToString());
-            }
-            finally
-            {
-                //Close and Dispose streamReader if not already done
-                CloseDisposeStreamReader(streamReader);
+                LunaLog.Error($"Error while changing settings: {e}");
             }
 
             return true;
         }
 
-        //Changes selected server option
-        private static void ChangeServerSettings(string settingName, string newValue, StreamReader streamReader, XmlDocument document, string settingsPath)
+        private static bool ValidateParameters(string[] parameters)
         {
-            //Set message
-            var msgSettingNameInfo = Environment.NewLine + "Setting name error:"
-                + Environment.NewLine + "'" + settingName + "'"
-                + Environment.NewLine + "is not a valid setting name."
-                + Environment.NewLine + "Setting name is case sensitive.";
-
-            //Load document
-            document.Load(streamReader);
-            //Try to find the xml node and save the new value to setting file
-            try
+            if (parameters == null)
             {
-                //Get node
-                var nodeSetting = document.SelectSingleNode("//" + settingName);
-                //Return if null
-                if (nodeSetting == null)
-                {
-                    LunaLog.Error(msgSettingNameInfo);
-                    return;
-                }
-                //Change node inner text
-                if (newValue == "-")
-                {
-                    nodeSetting.InnerText = "";
-                }
-                else
-                {
-                    nodeSetting.InnerText = newValue;
-                }
-                //Save document
-                SaveDocument(streamReader, document, settingsPath);
-                //Send info message that setting has changed
-                LunaLog.Normal(Environment.NewLine + "Setting changes saved." + Environment.NewLine + "You may need to restart the server for the changes to take effect.");
+                LunaLog.Error($"Syntax error. No parameters found.{Usage}");
+                return false;
             }
-            catch (Exception)
+
+            if (parameters.Length != 3)
             {
-                LunaLog.Error(msgSettingNameInfo);
+                LunaLog.Error($"Syntax error. Wrong number of parameters.{Usage}");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(parameters[0]))
+            {
+                LunaLog.Error($"Syntax error. First parameter not found.{Usage}");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(parameters[1]))
+            {
+                LunaLog.Error($"Syntax error. Second parameter not found.{Usage}");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(parameters[2]))
+            {
+                LunaLog.Error($"Syntax error. Third parameter not found.{Usage}");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static string GetFileName(string settingsFileParameter)
+        {
+            if (settingsFileParameter.ToLower() == "settings")
+            {
+                return "Settings.xml";
+            }
+
+            if (settingsFileParameter.ToLower() == "gameplaysettings")
+            {
+                return "GameplaySettings.xml";
+            }
+
+            if (settingsFileParameter.ToLower() == "debugsettings")
+            {
+                return "DebugSettings.xml";
+            }
+
+            return null;
+        }
+
+        private static void ChangeServerSettings(string settingName, string newValue, string settingsPath)
+        {
+            var document = new XmlDocument();
+            document.Load(settingsPath);
+
+            var nodeSetting = document.SelectSingleNode("//" + settingName);
+            if (nodeSetting == null)
+            {
+                LunaLog.Error($"\'{settingName}\' is not a valid setting name. Setting name is case sensitive.");
                 return;
             }
-        }
-        
-        //Saves changes to document
-        private static void SaveDocument(StreamReader streamReader, XmlDocument document, string settingsPath)
-        {
-            //Close and dispose streamReader to be able to save the document
-            CloseDisposeStreamReader(streamReader);
-            //Save document
-            document.Save(settingsPath);
-        }
 
-        //Closes and disposes the stream reader
-        private static void CloseDisposeStreamReader(StreamReader streamReader)
-        {
-            streamReader.Close();
-            streamReader.Dispose();
+            nodeSetting.InnerText = newValue == "-" ? "" : newValue;
+            document.Save(settingsPath);
+
+            LunaLog.Normal("Setting changes saved. You may need to restart the server for the changes to take effect.");
         }
     }
 }
