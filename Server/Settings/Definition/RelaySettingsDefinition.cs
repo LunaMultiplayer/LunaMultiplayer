99aaffa1442f6ea7210f1fdf91bb9cb39b9190ea
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-05-27 02:37:28 +0200

    Fix XML comment

--- Server/Settings/Definition/RelaySettingsDefinition.cs
+++ Server/Settings/Definition/RelaySettingsDefinition.cs
@@ -7,7 +7,7 @@ namespace Server.Settings.Definition
     [Serializable]
     public class RelaySettingsDefinition
     {
-        [XmlComment(Value = "Relay system mode. Dictionary uses more RAM but it's faster. DataBase use disk space instead but it's slower. Values: Dictionary, DataBase")]
+        [XmlComment(Value = "Relay system mode. Dictionary uses more RAM but it's faster. Database use disk space instead but it's slower. Values: Dictionary, Database")]
         public RelaySystemMode RelaySystemMode { get; set; } = RelaySystemMode.Dictionary;
 
         [XmlComment(Value = "Interval for saving POSITION updates IN THE SERVER so they are later sent to the OTHER players in the past. " +
