1ac0a9e85694cc0f95590c05f75d914b33247d00
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-05-29 22:55:23 +0200

    Added logging

--- Client/Systems/VesselProtoSys/VesselProtoEvents.cs
+++ Client/Systems/VesselProtoSys/VesselProtoEvents.cs
@@ -178,21 +178,27 @@ namespace LunaClient.Systems.VesselProtoSys
         }
 
         /// <summary>
-        /// Triggered when transmitting science. Science data is stored in the vessel so send it again to the server
+        /// Triggered when transmitting science. Science experiment is stored in the vessel so send the definition to the server
         /// </summary>
         public void TriggeredDataTransmission(ScienceData data0, Vessel data1, bool data2)
         {
             if (FlightGlobals.ActiveVessel != null && !VesselCommon.IsSpectating)
+            {
+                LunaLog.Log("Detected a experiment transmission. Sending vessel definition to the server");
                 System.MessageSender.SendVesselMessage(FlightGlobals.ActiveVessel, true);
+            }
         }
 
         /// <summary>
-        /// Triggered when storing science. Science data is stored in the vessel so send it again to the server
+        /// Triggered when storing science. Science experiment is stored in the vessel so send the definition to the server
         /// </summary>
         public void ExperimentStored(ScienceData data)
         {
             if (FlightGlobals.ActiveVessel != null && !VesselCommon.IsSpectating)
+            {
+                LunaLog.Log("Detected a experiment stored. Sending vessel definition to the server");
                 System.MessageSender.SendVesselMessage(FlightGlobals.ActiveVessel, true);
+            }
         }
     }
 }
