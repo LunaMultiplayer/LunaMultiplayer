6944332141c2486d69e91770479b8352ee0e8399
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-05-29 23:59:43 +0200

    Fix sharing experiments

--- Client/Systems/VesselProtoSys/VesselProtoEvents.cs
+++ Client/Systems/VesselProtoSys/VesselProtoEvents.cs
@@ -2,6 +2,7 @@
 using LunaClient.Localization;
 using LunaClient.Systems.Lock;
 using LunaClient.Systems.SettingsSys;
+using LunaClient.Systems.ShareScienceSubject;
 using LunaClient.Systems.VesselRemoveSys;
 using LunaClient.Utilities;
 using LunaClient.VesselStore;
@@ -180,24 +181,38 @@ namespace LunaClient.Systems.VesselProtoSys
         /// <summary>
         /// Triggered when transmitting science. Science experiment is stored in the vessel so send the definition to the server
         /// </summary>
-        public void TriggeredDataTransmission(ScienceData data0, Vessel data1, bool data2)
+        public void TriggeredDataTransmission(ScienceData science, Vessel vessel, bool data)
         {
             if (FlightGlobals.ActiveVessel != null && !VesselCommon.IsSpectating)
-            {
-                LunaLog.Log("Detected a experiment transmission. Sending vessel definition to the server");
-                System.MessageSender.SendVesselMessage(FlightGlobals.ActiveVessel, true);
+            {                
+                //We must send the science subject aswell!
+                var subject = ResearchAndDevelopment.GetSubjectByID(science.subjectID);
+                if (subject != null)
+                {
+                    LunaLog.Log("Detected a experiment transmission. Sending vessel definition to the server");
+                    System.MessageSender.SendVesselMessage(FlightGlobals.ActiveVessel, true);
+
+                    ShareScienceSubjectSystem.Singleton.MessageSender.SendScienceSubjectMessage(subject);
+                }
             }
         }
 
         /// <summary>
         /// Triggered when storing science. Science experiment is stored in the vessel so send the definition to the server
         /// </summary>
-        public void ExperimentStored(ScienceData data)
+        public void ExperimentStored(ScienceData science)
         {
             if (FlightGlobals.ActiveVessel != null && !VesselCommon.IsSpectating)
             {
-                LunaLog.Log("Detected a experiment stored. Sending vessel definition to the server");
-                System.MessageSender.SendVesselMessage(FlightGlobals.ActiveVessel, true);
+                //We must send the science subject aswell!
+                var subject = ResearchAndDevelopment.GetSubjectByID(science.subjectID);
+                if (subject != null)
+                {
+                    LunaLog.Log("Detected a experiment stored. Sending vessel definition to the server");
+                    System.MessageSender.SendVesselMessage(FlightGlobals.ActiveVessel, true);
+
+                    ShareScienceSubjectSystem.Singleton.MessageSender.SendScienceSubjectMessage(subject);
+                }
             }
         }
     }
