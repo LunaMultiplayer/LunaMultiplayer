670f528047e93fc51618c5654b11baa8aa2714a4
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-05-30 17:37:13 +0200

    Master server messages changed so updating the minor version

--- LMP.Tests/Properties/AssemblyInfo.cs
+++ LMP.Tests/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("51f95d0b-c12d-44a2-b390-f29e9491ff8f")]
 
-[assembly: AssemblyVersion("0.9.45")]
-[assembly: AssemblyFileVersion("0.9.45")]
-[assembly: AssemblyInformationalVersion("0.9.45-compiled")]
+[assembly: AssemblyVersion("0.10.45")]
+[assembly: AssemblyFileVersion("0.10.45")]
+[assembly: AssemblyInformationalVersion("0.10.45-compiled")]
