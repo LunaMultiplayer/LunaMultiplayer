ac8931d6d8a9fa9a75b5999f69f437cc48de0dc4
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-06-05 01:31:09 +0200

    Added new concurrent cached queue

---
+++ Client/Base/CachedConcurrentQueue.cs
@@ -0,0 +1,52 @@
+﻿using LunaCommon.Message.Interface;
+using System.Collections.Concurrent;
+
+namespace LunaClient.Base
+{
+    public abstract class CachedConcurrentQueue<T, TD> where T : new() where TD : IMessageData
+    {
+        protected readonly ConcurrentBag<T> _cache = new ConcurrentBag<T>();
+        protected ConcurrentQueue<T> _queue = new ConcurrentQueue<T>();
+
+        public int CacheSize => _cache.Count;
+        public int Count => _queue.Count;
+        public bool IsEmpty => _queue.IsEmpty;
+
+        public virtual void Enqueue(TD msgData)
+        {
+            if (_cache.TryTake(out var cachedValue))
+            {
+                AssignFromMessage(cachedValue, msgData);
+                _queue.Enqueue(cachedValue);
+            }
+            else
+            {
+                var newVal = new T();
+                AssignFromMessage(newVal, msgData);
+                _queue.Enqueue(newVal);
+            }
+        }
+
+        public virtual bool TryDequeue(out T result)
+        {
+            return _queue.TryDequeue(out result);
+        }
+
+        public virtual bool TryPeek(out T result)
+        {
+            return _queue.TryPeek(out result);
+        }
+
+        public virtual void Clear()
+        {
+            while (!_queue.IsEmpty && _queue.TryDequeue(out _)) { }
+        }
+
+        public virtual void Recycle(T item)
+        {
+            if (item != null) _cache.Add(item);
+        }
+
+        protected abstract void AssignFromMessage(T value, TD msgData);
+    }
+}
---
+++ Client/Systems/VesselPositionSys/PositionUpdateQueue.cs
@@ -0,0 +1,56 @@
+﻿using LunaClient.Base;
+using LunaClient.Systems.Warp;
+using LunaCommon.Message.Data.Vessel;
+using System;
+
+namespace LunaClient.Systems.VesselPositionSys
+{
+    public class PositionUpdateQueue : CachedConcurrentQueue<VesselPositionUpdate, VesselPositionMsgData>
+    {
+        private const float MaxTimeDiff = 2f;
+
+        public override bool TryDequeue(out VesselPositionUpdate result)
+        {
+            return KeepDequeuing(out result);
+        }
+
+        private bool KeepDequeuing(out VesselPositionUpdate result)
+        {
+            var dequeueResult = base.TryDequeue(out result);
+
+            if (dequeueResult)
+            {
+                if (WarpSystem.Singleton.CurrentlyWarping || WarpSystem.Singleton.WaitingSubspaceIdFromServer || result.SubspaceId == -1) return true;
+
+                if (!WarpSystem.Singleton.SubspaceIdIsMoreAdvancedInTime(result.SubspaceId) && Count > 5)
+                {
+                    dequeueResult = KeepDequeuing(out result);
+                }
+                else if (WarpSystem.Singleton.CurrentSubspaceTime - result.GameTimeStamp > MaxTimeDiff)
+                {
+                    dequeueResult = KeepDequeuing(out result);
+                }
+            }
+
+            return dequeueResult;
+        }
+
+        protected override void AssignFromMessage(VesselPositionUpdate value, VesselPositionMsgData msgData)
+        {
+            value.VesselId = msgData.VesselId;
+            value.SubspaceId = msgData.SubspaceId;
+            value.BodyIndex = msgData.BodyIndex;
+            value.HeightFromTerrain = msgData.HeightFromTerrain;
+            value.Landed = msgData.Landed;
+            value.Splashed = msgData.Splashed;
+            value.GameTimeStamp = msgData.GameTime;
+            value.HackingGravity = msgData.HackingGravity;
+
+            Array.Copy(msgData.SrfRelRotation, value.SrfRelRotation, 4);
+            Array.Copy(msgData.Velocity, value.Velocity, 3);
+            Array.Copy(msgData.LatLonAlt, value.LatLonAlt, 3);
+            Array.Copy(msgData.NormalVector, value.NormalVector, 3);
+            Array.Copy(msgData.Orbit, value.Orbit, 8);
+        }
+    }
+}
