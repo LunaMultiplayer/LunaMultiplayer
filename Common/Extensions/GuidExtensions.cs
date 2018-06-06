d611b226633057accc9aba1a85de48a94729bef3
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-06-06 17:44:57 +0200

    Added Guid extension

---
+++ Common/Extensions/GuidExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LunaCommon.Extensions
+{
+    public static class GuidExtensions
+    {
+        public static string ToSmallString(this Guid value)
+        {
+            return value.ToString("D").Substring(0, 8);
+        }
+    }
+}
