1120434ceb24be89030972b82240b7057d4ad17a
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-06-12 10:42:50 +0200

    Fix casting

--- Server/Web/Structures/Settings/ServerDebugSettings.cs
+++ Server/Web/Structures/Settings/ServerDebugSettings.cs
@@ -4,8 +4,8 @@ namespace Server.Web.Structures.Settings
 {
     public class ServerDebugSettings
     {
-        public int SimulatedLossChance => DebugSettings.SettingsStore.SimulatedLossChance;
-        public int SimulatedDuplicatesChance => DebugSettings.SettingsStore.SimulatedDuplicatesChance;
+        public float SimulatedLossChance => DebugSettings.SettingsStore.SimulatedLossChance;
+        public float SimulatedDuplicatesChance => DebugSettings.SettingsStore.SimulatedDuplicatesChance;
         public int MaxSimulatedRandomLatencyMs => DebugSettings.SettingsStore.MaxSimulatedRandomLatencyMs;
         public int MinSimulatedLatencyMs => DebugSettings.SettingsStore.MinSimulatedLatencyMs;
     }
