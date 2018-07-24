ba8d2fa0c38afdd0d6ae364d8c6c5b096d9a42ab
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-07-24 11:16:50 +0200

    Added a floating origin system

---
+++ Client/Systems/FloatingOrigin/FloatingOriginEvents.cs
@@ -0,0 +1,13 @@
+﻿using LunaClient.Base;
+
+namespace LunaClient.Systems.FloatingOrigin
+{
+    public class FloatingOriginEvents : SubSystem<FloatingOriginSystem>
+    {
+        public void FloatingOriginShift(Vector3d offset, Vector3d offsetNonKrakensbane)
+        {
+            FloatingOriginSystem.Offset = offset;
+            FloatingOriginSystem.OffsetNonKrakensbane = offsetNonKrakensbane;
+        }
+    }
+}
---
+++ Client/Systems/FloatingOrigin/FloatingOriginSystem.cs
@@ -0,0 +1,32 @@
+﻿namespace LunaClient.Systems.FloatingOrigin
+{
+    public class FloatingOriginSystem : Base.System<FloatingOriginSystem>
+    {
+        #region Fields & properties
+
+        public static Vector3d Offset { get; set; }
+        public static Vector3d OffsetNonKrakensbane { get; set; }
+
+        private FloatingOriginEvents FloatingOriginEvents { get; } = new FloatingOriginEvents();
+        
+        #endregion
+
+        #region Base overrides
+
+        public override string SystemName { get; } = nameof(FloatingOriginSystem);
+
+        protected override void OnEnabled()
+        {
+            base.OnEnabled();
+            GameEvents.onFloatingOriginShift.Add(FloatingOriginEvents.FloatingOriginShift);
+        }
+
+        protected override void OnDisabled()
+        {
+            base.OnDisabled();
+            GameEvents.onFloatingOriginShift.Remove(FloatingOriginEvents.FloatingOriginShift);
+        }
+
+        #endregion
+    }
+}
