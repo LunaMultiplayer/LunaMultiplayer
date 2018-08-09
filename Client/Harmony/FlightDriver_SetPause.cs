a0815661335990393de3dbe29e6759f7e384868c
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-08-09 15:10:25 +0200

    Added a harmony patch to disable pausing

---
+++ Client/Harmony/FlightDriver_SetPause.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using LunaCommon.Enums;
+// ReSharper disable All
+
+namespace LunaClient.Harmony
+{
+    /// <summary>
+    /// This harmony patch is intended to disable pausing while in LMP
+    /// </summary>
+    [HarmonyPatch(typeof(FlightDriver))]
+    [HarmonyPatch("SetPause")]
+    public class FlightDriver_SetPause
+    {
+        [HarmonyPrefix]
+        private static bool PrefixSetPause(bool pauseState, bool postScreenMessage)
+        {
+            if (MainSystem.NetworkState < ClientState.Connected) return true;
+
+            return !pauseState;
+        }
+    }
+}
