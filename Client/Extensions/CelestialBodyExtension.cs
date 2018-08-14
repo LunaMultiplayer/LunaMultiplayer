a80e8aa01827461c6e31c28528d944fce3aa229b
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-08-14 15:50:08 +0200

    Fix NRE

--- Client/Extensions/CelestialBodyExtension.cs
+++ Client/Extensions/CelestialBodyExtension.cs
@@ -6,6 +6,8 @@ namespace LunaClient.Extensions
     {
         public static double SiderealDayLength(this CelestialBody body)
         {
+            if (body == null) return 0;
+
             var siderealOrbitalPeriod = 6.28318530717959 * Math.Sqrt(Math.Pow(Math.Abs(body.orbit.semiMajorAxis), 3) / body.orbit.referenceBody.gravParameter);
             return body.rotationPeriod * siderealOrbitalPeriod / (siderealOrbitalPeriod + body.rotationPeriod);
         }
