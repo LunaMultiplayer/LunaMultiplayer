ac4e3bd575deb00cee63ac781285059ea4f4af69
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-09-05 13:47:52 +0200

    Restored the vessel freeze unfreeze

---
+++ Client/Extensions/VesselExtension.cs
@@ -0,0 +1,36 @@
+﻿using LunaClient.Systems.VesselFreezeSys;
+using UnityEngine;
+
+namespace LunaClient.Extensions
+{
+    public static class VesselExtension
+    {
+        /// <summary>
+        /// Freeze a vessel position
+        /// </summary>
+        public static void FreezePosition(this Vessel vessel)
+        {
+            if (vessel != null && !vessel.packed && vessel.parts.Count > 0)
+            {
+                if (vessel.rootPart?.Rigidbody?.constraints == RigidbodyConstraints.None)
+                {
+                    vessel.parts?.ForEach(p => p.Rigidbody.constraints = RigidbodyConstraints.FreezeAll);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Freeze a vessel position
+        /// </summary>
+        public static void UnfreezePosition(this Vessel vessel)
+        {
+            if (vessel != null && !vessel.packed && vessel.parts.Count > 0)
+            {
+                if (vessel.rootPart?.Rigidbody?.constraints == RigidbodyConstraints.FreezeAll)
+                {
+                    vessel.parts?.ForEach(p => p.Rigidbody.constraints = RigidbodyConstraints.None);
+                }
+            }
+        }
+    }
+}
