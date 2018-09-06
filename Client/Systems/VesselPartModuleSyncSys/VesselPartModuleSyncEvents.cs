b9fc5768326c24d3838d95ced411621ef17316d8
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-09-06 12:43:53 +0200

    Optimization

--- Client/Systems/VesselPartModuleSyncSys/VesselPartModuleSyncEvents.cs
+++ Client/Systems/VesselPartModuleSyncSys/VesselPartModuleSyncEvents.cs
@@ -23,7 +23,7 @@ namespace LunaClient.Systems.VesselPartModuleSyncSys
                         if (definition.PersistentModuleField.TryGetValue(fieldName, out var fieldInfo))
                         {
                             var customizationResult = CustomizationsHandler.SkipModule(vessel.id, part.flightID, baseModuleName, fieldInfo.Name, false, out _);
-                            if (customizationResult == CustomizationResult.Ignore)
+                            if (customizationResult != CustomizationResult.Ok)
                                 return;
 
                             var fieldVal = fieldInfo.GetValue(module).ToInvariantString();
@@ -31,17 +31,12 @@ namespace LunaClient.Systems.VesselPartModuleSyncSys
 
                             if (snapshotVal != null && fieldVal != null && fieldVal != snapshotVal)
                             {
-                                switch (customizationResult)
-                                {
-                                    case CustomizationResult.TooEarly: //Do not update anything and wait until next time
-                                        break;
-                                    case CustomizationResult.Ok:
-                                        module.snapshot?.moduleValues?.SetValue(fieldInfo.Name, fieldVal);
-                                        LunaLog.Log($"Detected a part module change. FlightId: {part.flightID} PartName: {part.name} Module: {module.moduleName} BaseModule: {baseModuleName} " +
-                                                    $"Field: {fieldInfo.Name} ValueBefore: {snapshotVal} ValueNow: {fieldVal}");
-                                        System.MessageSender.SendVesselPartSyncMsg(vessel.id, part.flightID, module.moduleName, baseModuleName, fieldInfo.Name, fieldVal);
-                                        break;
-                                }
+                                module.snapshot?.moduleValues?.SetValue(fieldInfo.Name, fieldVal);
+
+                                LunaLog.Log($"Detected a part module change. FlightId: {part.flightID} PartName: {part.name} Module: {module.moduleName} BaseModule: {baseModuleName} " +
+                                            $"Field: {fieldInfo.Name} ValueBefore: {snapshotVal} ValueNow: {fieldVal}");
+
+                                System.MessageSender.SendVesselPartSyncMsg(vessel.id, part.flightID, module.moduleName, baseModuleName, fieldInfo.Name, fieldVal);
                             }
                         }
                     }
