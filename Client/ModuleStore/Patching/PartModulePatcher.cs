bfeb33bf766d2245ef34f81a9179deff8bc0d720
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-09-12 13:49:41 +0200

    Added field customization to the transpiler

--- Client/ModuleStore/Patching/PartModulePatcher.cs
+++ Client/ModuleStore/Patching/PartModulePatcher.cs
@@ -1,4 +1,8 @@
 ﻿using Harmony;
+using Harmony.ILCopying;
+using LunaClient.Base;
+using LunaClient.ModuleStore.Harmony;
+using LunaClient.ModuleStore.Structures;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +16,21 @@ namespace LunaClient.ModuleStore.Patching
     /// </summary>
     public partial class PartModulePatcher
     {
+        /// <summary>
+        /// Here we store the original method instructions in case we fuck things up
+        /// </summary>
         private static readonly List<CodeInstruction> InstructionsBackup = new List<CodeInstruction>();
+
+        /// <summary>
+        /// Here we will have the IL code of the method <see cref="TestModule.ExampleFieldChangeCall"/>
+        /// </summary>
+        private static readonly List<CodeInstruction> FieldChangeCallInstructions = new List<CodeInstruction>();
 
         #region Transpilers references
 
         private static readonly HarmonyMethod RestoreTranspilerMethod = new HarmonyMethod(typeof(PartModulePatcher).GetMethod(nameof(Restore)));
-
+        private static readonly HarmonyMethod InitFieldChangeTranspilerMethod = new HarmonyMethod(typeof(PartModulePatcher).GetMethod(nameof(InitFieldChangeCallInstructions)));
+        private static readonly HarmonyMethod FieldChangeTranspilerMethod = new HarmonyMethod(typeof(PartModulePatcher).GetMethod(nameof(FieldChangeTranspiler)));
         #endregion
 
         /// <summary>
@@ -25,6 +38,7 @@ namespace LunaClient.ModuleStore.Patching
         /// </summary>
         public static void Awake()
         {
+            HarmonyPatcher.HarmonyInstance.Patch(TestModule.ExampleFieldChangeCallMethod, null, null, InitFieldChangeTranspilerMethod);
             foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
                 try
@@ -36,7 +50,7 @@ namespace LunaClient.ModuleStore.Patching
                         {
                             if (FieldModuleStore.CustomizedModuleBehaviours.TryGetValue(partModule.Name, out var definition))
                             {
-                                PatchMethods(partModule, definition);
+                                PatchFieldsAndMethods(partModule, definition);
                             }
                         }
                         catch (Exception ex)
@@ -53,35 +67,124 @@ namespace LunaClient.ModuleStore.Patching
         }
 
         /// <summary>
+        /// Patches the ACTION methods defined in the XML with the transpiler
+        /// </summary>
+        private static void PatchFieldsAndMethods(Type partModule, ModuleDefinition definition)
+        {
+            foreach (var partModuleMethod in partModule.GetMethods(AccessTools.all))
+            {
+                if (definition.Fields.Any() && MethodSetsCustomizedField(partModuleMethod, definition))
+                {
+                    var patchMethodCalls = definition.Methods.Any(m => m.MethodName == partModuleMethod.Name);
+                    try
+                    {
+                        LunaLog.Log(patchMethodCalls
+                            ? $"Patching method {partModuleMethod.Name} for field changes and method call in module {partModule.Name} of assembly {partModule.Assembly.FullName}"
+                            : $"Patching method {partModuleMethod.Name} for field changes in module {partModule.Name} of assembly {partModule.Assembly.FullName}");
+
+                        HarmonyPatcher.HarmonyInstance.Patch(partModuleMethod, null, patchMethodCalls ? HarmonyCustomMethod.MethodPostfixMethod : null,
+                            FieldChangeTranspilerMethod);
+                    }
+                    catch
+                    {
+                        LunaLog.LogError($"Could not patch method {partModuleMethod.Name} for field changes in module {partModule.Name} of assembly {partModule.Assembly.FullName}");
+                        HarmonyPatcher.HarmonyInstance.Patch(partModuleMethod, null, patchMethodCalls ? HarmonyCustomMethod.MethodPostfixMethod : null, 
+                            RestoreTranspilerMethod);
+                    }
+                }
+                else if (definition.Methods.Any(m => m.MethodName == partModuleMethod.Name))
+                {
+                    if (partModuleMethod.GetParameters().Any())
+                    {
+                        LunaLog.LogError($"Method {partModuleMethod.Name} is not valid as it doesn't have a parameterless signature");
+                        continue;
+                    }
+
+                    LunaLog.Log($"Patching method {partModuleMethod.Name} for method call in module {partModule.Name} of assembly {partModule.Assembly.FullName}");
+                    HarmonyPatcher.HarmonyInstance.Patch(partModuleMethod, null, HarmonyCustomMethod.MethodPostfixMethod);
+                }
+            }
+        }
+
+        /// <summary>
         /// This method restores a failed method patch
         /// </summary>
         public static IEnumerable<CodeInstruction> Restore(IEnumerable<CodeInstruction> instructions)
         {
             return InstructionsBackup.AsEnumerable();
         }
-        
+
         /// <summary>
-        /// This method takes the IL code of the part module and appends the ActionCallInstructions at the end
+        /// This is the init transpiler. It will take the instructions of the ExampleCall method and store them in the
+        /// <see cref="FieldChangeCallInstructions"/> list so we can use them later.
         /// </summary>
-        public static IEnumerable<CodeInstruction> AppendInstructions(MethodBase originalMethod, IEnumerable<CodeInstruction> instructions, List<CodeInstruction> afterPatch)
+        public static IEnumerable<CodeInstruction> InitFieldChangeCallInstructions(IEnumerable<CodeInstruction> instructions)
+        {
+            //We only take the first 4 instructions as the other ones are the return
+            FieldChangeCallInstructions.AddRange(instructions.Take(4));
+            return FieldChangeCallInstructions;
+        }
+
+        /// <summary>
+        /// This method takes the IL code of the part module. If it finds that you're changing the value (OpCodes.Ldstr) of a field that has the attribute "KSPField" and that
+        /// attribute has the "isPersistant" then we add the FieldChangeCallInstructions IL opcodes just after it so the event is triggered.
+        /// </summary>
+        public static IEnumerable<CodeInstruction> FieldChangeTranspiler(IEnumerable<CodeInstruction> instructions)
         {
             InstructionsBackup.Clear();
 
             var codes = new List<CodeInstruction>(instructions);
             InstructionsBackup.AddRange(codes);
 
-            for (var i = 0; i < afterPatch.Count; i++)
+            var length = codes.Count;
+            for (var i = 0; i < length; i++)
             {
-                if (afterPatch[i].opcode == OpCodes.Ldstr)
+                //This is the case when a field value is being set
+                if (codes[i].opcode == OpCodes.Stfld)
                 {
-                    //Change the name operand so the proper "method name" is shown
-                    afterPatch[i].operand = originalMethod.Name;
+                    if (!(codes[i].operand is FieldInfo operand)) continue;
+
+                    var attributes = operand.GetCustomAttributes(typeof(KSPField), false).Cast<KSPField>().ToArray();
+                    if (attributes.Any() && attributes.First().isPersistant)
+                    {
+                        for (var j = 0; j < FieldChangeCallInstructions.Count; j++)
+                        {
+                            if (FieldChangeCallInstructions[j].opcode == OpCodes.Ldstr)
+                            {
+                                //Change the name operand so the proper "field name" is shown
+                                FieldChangeCallInstructions[j].operand = operand.Name;
+                            }
+
+                            codes.Insert(i + 1 + j, new CodeInstruction(FieldChangeCallInstructions[j]));
+                        }
+
+                        length += FieldChangeCallInstructions.Count;
+                        i += FieldChangeCallInstructions.Count;
+                    }
                 }
             }
 
-            codes.InsertRange(codes.Count - 1, afterPatch);
-
             return codes.AsEnumerable();
+        }
+
+        /// <summary>
+        /// Checks if the given method has a IL instruction that SETS (therefore, it changes the value) a customized field
+        /// </summary>
+        private static bool MethodSetsCustomizedField(MethodBase partModuleMethod, ModuleDefinition definition)
+        {
+            var method = DynamicTools.CreateDynamicMethod(partModuleMethod, "read");
+            var instructions = MethodBodyReader.GetInstructions(method.GetILGenerator(), partModuleMethod);
+
+            //OpCodes.Stfld is the opcode for SETTING the value of a field
+            foreach (var instruction in instructions.Where(i => i.opcode == OpCodes.Stfld))
+            {
+                if (!(instruction.operand is FieldInfo operand)) continue;
+
+                if (definition.Fields.Any(f => f.FieldName == operand.Name))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
