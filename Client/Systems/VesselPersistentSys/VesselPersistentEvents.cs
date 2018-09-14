ad775dcecd42eff01ef815715b2218969bf5c02b
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-09-14 18:09:46 +0200

    Do not send new persistent id's on not owned vessels

--- Client/Systems/VesselPersistentSys/VesselPersistentEvents.cs
+++ Client/Systems/VesselPersistentSys/VesselPersistentEvents.cs
@@ -1,4 +1,8 @@
 ﻿using LunaClient.Base;
+using LunaClient.Systems.Lock;
+using LunaClient.Systems.SettingsSys;
+using LunaClient.Systems.VesselRemoveSys;
+using LunaClient.VesselUtilities;
 
 namespace LunaClient.Systems.VesselPersistentSys
 {
@@ -6,12 +10,37 @@ namespace LunaClient.Systems.VesselPersistentSys
     {
         public void PartPersistentIdChanged(uint vesselPersistentId, uint from, uint to)
         {
-            System.MessageSender.SendPartPersistantIdChanged(vesselPersistentId, from, to);
+            var vessel = GetChangingVesselLock(vesselPersistentId);
+            if (vessel == null) return;
+
+            if (LockSystem.LockQuery.UnloadedUpdateLockBelongsToPlayer(vessel.id, SettingsSystem.CurrentSettings.PlayerName) || 
+                FlightGlobals.ActiveVessel == vessel && !VesselCommon.IsSpectating)
+                System.MessageSender.SendPartPersistantIdChanged(vesselPersistentId, from, to);
+            else
+            {
+                VesselRemoveSystem.Singleton.KillVessel(vessel, "Changed persistent id on a not owned vessel");
+            }
         }
 
         public void VesselPersistentIdChanged(uint from, uint to)
         {
-            System.MessageSender.SendVesselPersistantIdChanged(from, to);
+            var vessel = GetChangingVesselLock(to);
+            if (vessel == null) vessel = GetChangingVesselLock(from);
+            if (vessel == null) return;
+
+            if (LockSystem.LockQuery.UnloadedUpdateLockBelongsToPlayer(vessel.id, SettingsSystem.CurrentSettings.PlayerName) ||
+                FlightGlobals.ActiveVessel == vessel && !VesselCommon.IsSpectating)
+                System.MessageSender.SendVesselPersistantIdChanged(from, to);
+            else
+            {
+                VesselRemoveSystem.Singleton.KillVessel(vessel, "Changed persistent id on a not owned vessel");
+            }
         }
+
+        private Vessel GetChangingVesselLock(uint vesselPersistentId)
+        {
+            return FlightGlobals.FindVessel(vesselPersistentId, out var foundVessel) ? foundVessel : null;
+        }
+
     }
 }
