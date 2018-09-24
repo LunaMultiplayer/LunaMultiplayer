174a5b471570dac2109deab3c9eda0b9c24c65e2
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-09-25 01:43:53 +0200

    Fix the issues with the persistentId and vessels dissapearing, etc

---
+++ LmpClient/Harmony/ShipConstruct_LoadShip.cs
@@ -0,0 +1,31 @@
+﻿using Harmony;
+using LmpCommon.Enums;
+// ReSharper disable All
+
+namespace LmpClient.Harmony
+{
+    /// <summary>
+    /// This harmony patch is intended to always assign a persistentID and NEVER use the persistent id of the .craft file
+    /// </summary>
+    [HarmonyPatch(typeof(ShipConstruct))]
+    [HarmonyPatch("LoadShip")]
+    [HarmonyPatch(new[] { typeof(ConfigNode), typeof(uint) })]
+    public class ShipConstruct_LoadShip
+    {
+        [HarmonyPrefix]
+        private static void PrefixLoadShip(ConfigNode root, ref uint persistentID)
+        {
+            if (MainSystem.NetworkState < ClientState.Connected) return;
+
+            if (persistentID == 0)
+            {
+                persistentID = FlightGlobals.GetUniquepersistentId();
+
+                foreach (var part in root.GetNodes("PART"))
+                {
+                    part.SetValue("persistentId", FlightGlobals.GetUniquepersistentId(), false);
+                }
+            }
+        }
+    }
+}
