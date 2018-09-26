f20e7a6ec0f9ed15e363b71289ddc60677527f85
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-09-27 01:28:52 +0200

    added a patch to ignore wheel stress on vessels that are not ours

---
+++ LmpClient/Harmony/ModuleWheelDamage_FixedUpdate.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using LmpCommon.Enums;
+using ModuleWheels;
+
+// ReSharper disable All
+
+namespace LmpClient.Harmony
+{
+    /// <summary>
+    /// This harmony patch is intended to skip the damage in a wheel if the vessel is not ours
+    /// </summary>
+    [HarmonyPatch(typeof(ModuleWheelDamage))]
+    [HarmonyPatch("FixedUpdate")]
+    public class ModuleWheelDamage_FixedUpdate
+    {
+        [HarmonyPrefix]
+        private static bool PrefixFixedUpdate(ModuleWheelDamage __instance)
+        {
+            if (MainSystem.NetworkState < ClientState.Connected) return true;
+
+            return __instance.part.crashTolerance != float.PositiveInfinity;
+        }
+    }
+}
