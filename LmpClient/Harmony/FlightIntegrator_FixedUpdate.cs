499aab3be7899f522711841f8f8e05d82feb121f
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-09-28 11:35:03 +0200

    Skip flight integrator on vessels that are not ours

---
+++ LmpClient/Harmony/FlightIntegrator_FixedUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Harmony;
+using LmpCommon.Enums;
+// ReSharper disable All
+
+namespace LmpClient.Harmony
+{
+    /// <summary>
+    /// This harmony patch is intended to skip the FlightIntegrator on vessels that are not ours
+    /// </summary>
+    [HarmonyPatch(typeof(FlightIntegrator))]
+    [HarmonyPatch("FixedUpdate")]
+    public class FlightIntegrator_FixedUpdate
+    {
+        [HarmonyPrefix]
+        private static bool PrefixFixedUpdate(FlightIntegrator __instance)
+        {
+            if (MainSystem.NetworkState < ClientState.Connected) return true;
+            if (__instance.Vessel == FlightGlobals.ActiveVessel) return true;
+            
+            return __instance.Vessel.rootPart == null || !float.IsPositiveInfinity(__instance.Vessel.rootPart.crashTolerance);
+        }
+    }
+}
