5b97068d4bae25ff4e8583d457154e6091f667e3
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-10-11 15:10:49 +0200

    Let jatwaa test it....

--- LmpGlobal/KSPCompatible.cs
+++ LmpGlobal/KSPCompatible.cs
@@ -5,6 +5,6 @@ namespace LmpGlobal
     public static class KspCompatible
     {
         public static readonly Version MinKspVersion = new Version("1.4.3");
-        public static readonly Version MaxKspVersion = new Version("1.4.9");
+        public static readonly Version MaxKspVersion = new Version("1.5.9");
     }
 }
