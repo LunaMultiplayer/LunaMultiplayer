3b068dc08d1ac6c42682711236b7c9f78a826ddf
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-10-19 00:05:34 +0200

    Made LMP only compatible with 1.5.0

--- LmpGlobal/KSPCompatible.cs
+++ LmpGlobal/KSPCompatible.cs
@@ -4,7 +4,7 @@ namespace LmpGlobal
 {
     public static class KspCompatible
     {
-        public static readonly Version MinKspVersion = new Version("1.4.3");
+        public static readonly Version MinKspVersion = new Version("1.5.0");
         public static readonly Version MaxKspVersion = new Version("1.5.9");
     }
 }
