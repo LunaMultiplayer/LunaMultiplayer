d9e03e4ed7fcbba3ae0cf07e6ab42fe61a5575af
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-10-23 14:12:09 +0200

    Fix harmony patch

--- LmpClient/Harmony/OrbitDriver_TrackRigidbody.cs
+++ LmpClient/Harmony/OrbitDriver_TrackRigidbody.cs
@@ -6,7 +6,7 @@ using LmpCommon.Enums;
 namespace LmpClient.Harmony
 {
     /// <summary>
-    /// This harmony patch is intended to call TrackRigidbody when the vessel is kinematic
+    /// This harmony patch is intended to call TrackRigidbody correctly when the vessel is kinematic
     /// If you don't run this patch, then the vessels controlled by other players will have an incorrect obt speed.
     /// This means that when spectating they will shake and when trying to grab a ladder your kerbal will be sent off
     /// </summary>
@@ -14,31 +14,35 @@ namespace LmpClient.Harmony
     [HarmonyPatch("TrackRigidbody")]
     public class OrbitDriver_TrackRigidbody
     {
-        [HarmonyPostfix]
-        private static void PostFixTrackRigidbody(OrbitDriver __instance, CelestialBody refBody, double fdtOffset, ref double ___updateUT)
+        /// <summary>
+        /// For the prefix we set the vessel as NON kinematic if it's controlled / updated by another player so the TrackRigidbody works as expected
+        /// </summary>
+        [HarmonyPrefix]
+        private static void PrefixTrackRigidbody(OrbitDriver __instance, bool __state)
         {
             if (MainSystem.NetworkState < ClientState.Connected) return;
-            if (__instance.updateMode == OrbitDriver.UpdateMode.IDLE) return;
-            if (__instance.vessel == null || __instance.vessel.rootPart == null || __instance.vessel.rootPart.rb == null || !__instance.vessel.rootPart.rb.isKinematic) return;
 
-            ___updateUT += fdtOffset;
-            __instance.vel = __instance.vessel.velocityD.xzy + __instance.orbit.GetRotFrameVelAtPos(__instance.referenceBody, __instance.pos);
+            __state = false;
 
-            if (refBody != __instance.referenceBody)
+            if (__instance.vessel && __instance.vessel.rootPart && __instance.vessel.rootPart.rb && __instance.vessel.rootPart.rb.isKinematic)
             {
-                if (__instance.vessel != null)
-                {
-                    var vector3d = __instance.vessel.CoMD - refBody.position;
-                    __instance.pos = vector3d.xzy;
-                }
-                var frameVel = __instance;
-                frameVel.vel = frameVel.vel + (__instance.referenceBody.GetFrameVel() - refBody.GetFrameVel());
+                __instance.vessel.rootPart.rb.isKinematic = false;
+                __state = true;
             }
+        }
 
-            __instance.lastTrackUT = ___updateUT;
-            __instance.orbit.UpdateFromStateVectors(__instance.pos, __instance.vel, refBody, ___updateUT);
-            __instance.pos.Swizzle();
-            __instance.vel.Swizzle();
+        /// <summary>
+        /// After the orbit calculations are done we put the vessel back to kinematic mode if needed
+        /// </summary>
+        [HarmonyPostfix]
+        private static void PostfixTrackRigidbody(OrbitDriver __instance, bool __state)
+        {
+            if (MainSystem.NetworkState < ClientState.Connected) return;
+
+            if (__instance.vessel && __instance.vessel.rootPart && __instance.vessel.rootPart.rb && !__instance.vessel.rootPart.rb.isKinematic && __state)
+            { 
+                __instance.vessel.rootPart.rb.isKinematic = true;
+            }
         }
     }
 }
