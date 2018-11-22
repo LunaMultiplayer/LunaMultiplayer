63fcdb6f9bddf75533397a956e4adbb4e1058776
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-11-23 00:52:50 +0100

    Nightly version numbering

--- LmpUpdater/Properties/AssemblyInfo.cs
+++ LmpUpdater/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("35a57b77-236e-492c-b003-1f8fc2f544f3")]
 
-[assembly: AssemblyVersion("0.16.0")]
-[assembly: AssemblyFileVersion("0.16.0")]
-[assembly: AssemblyInformationalVersion("0.16.0-compiled")]
+[assembly: AssemblyVersion("0.16.999")]
+[assembly: AssemblyFileVersion("0.16.999")]
+[assembly: AssemblyInformationalVersion("0.16.999-compiled")]
