b83d7ff0a952ce2f2a7b41b0cf18734eb69b84df
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-11-30 09:51:09 +0100

    Better decouple event than the one from KSP

---
+++ LmpClient/Events/PartEvent.cs
@@ -0,0 +1,13 @@
+﻿using LmpClient.Events.Base;
+
+// ReSharper disable All
+#pragma warning disable IDE1006
+
+namespace LmpClient.Events
+{
+    public class PartEvent : LmpBaseEvent
+    {
+        public static EventData<Part, float> onPartDecoupling;
+        public static EventData<Part, float, Vessel> onPartDecoupled;
+    }
+}
---
+++ LmpClient/Harmony/Part_Decouple.cs
@@ -0,0 +1,28 @@
+﻿using Harmony;
+using LmpClient.Events;
+
+// ReSharper disable All
+
+namespace LmpClient.Harmony
+{
+    /// <summary>
+    /// This harmony patch is intended to trigger an event when decoupling a part
+    /// </summary>
+    [HarmonyPatch(typeof(Part))]
+    [HarmonyPatch("decouple")]
+    public class Part_Decouple
+    {
+        [HarmonyPrefix]
+        private static void PrefixDecouple(Part __instance, float breakForce, Vessel __state)
+        {
+            __state = __instance.vessel;
+            PartEvent.onPartDecoupling.Fire(__instance, breakForce);
+        }
+
+        [HarmonyPostfix]
+        private static void PostfixDecouple(Part __instance, float breakForce, Vessel __state)
+        {
+            PartEvent.onPartDecoupled.Fire(__instance, breakForce, __state);
+        }
+    }
+}
