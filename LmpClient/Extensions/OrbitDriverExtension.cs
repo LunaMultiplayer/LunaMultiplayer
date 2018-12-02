176e4fc3fe287898b3e6ed725e7e94e9ba37609f
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-12-02 02:01:00 +0100

    Added force start

--- LmpClient/Extensions/OrbitDriverExtension.cs
+++ LmpClient/Extensions/OrbitDriverExtension.cs
@@ -7,9 +7,16 @@ namespace LmpClient.Extensions
     {
         private static readonly FieldInfo OrbitDriverReady = typeof(OrbitDriver).GetField("ready", AccessTools.all);
 
+        private static readonly MethodInfo OrbitDriverStart = typeof(OrbitDriver).GetMethod("Start", AccessTools.all);
+
         public static bool Ready(this OrbitDriver driver)
         {
             return (bool)OrbitDriverReady.GetValue(driver);
+        }
+
+        public static void ForceStart(this OrbitDriver driver)
+        {
+            OrbitDriverStart.Invoke(driver, null);
         }
     }
 }
