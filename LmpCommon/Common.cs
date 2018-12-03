c725abf69c3c94dd43968fbd679b5775ef5dd14a
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-12-03 14:09:18 +0100

    Protect against bad parsed endpoints. Closes #253

--- LmpCommon/Common.cs
+++ LmpCommon/Common.cs
@@ -94,12 +94,19 @@ namespace LmpCommon
 
         public static IPEndPoint CreateEndpointFromString(string endpoint)
         {
-            if (IPAddress.TryParse(endpoint.Split(':')[0], out var ip))
+            try
             {
-                return new IPEndPoint(ip, int.Parse(endpoint.Split(':')[1]));
-            }
+                if (IPAddress.TryParse(endpoint.Split(':')[0], out var ip))
+                {
+                    return new IPEndPoint(ip, int.Parse(endpoint.Split(':')[1]));
+                }
 
-            return new IPEndPoint(Dns.GetHostAddresses(endpoint.Split(':')[0])[0], int.Parse(endpoint.Split(':')[1]));
+                return new IPEndPoint(Dns.GetHostAddresses(endpoint.Split(':')[0])[0], int.Parse(endpoint.Split(':')[1]));
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         public static IPAddress GetIpFromString(string server)
