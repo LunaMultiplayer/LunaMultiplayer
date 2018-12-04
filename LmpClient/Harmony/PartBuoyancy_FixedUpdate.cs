c277bc7210bb2dc4fdd183bb3e6f0e46962d4e1f
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-12-04 15:40:46 +0100

    Added part buoyancy check

---
+++ LmpClient/Harmony/PartBuoyancy_FixedUpdate.cs
@@ -0,0 +1,32 @@
+﻿using Harmony;
+using LmpClient.Extensions;
+using LmpClient.Systems.VesselPositionSys;
+using LmpCommon.Enums;
+
+// ReSharper disable All
+
+namespace LmpClient.Harmony
+{
+    /// <summary>
+    /// This harmony patch is intended to avoid run the part checks in vessels that are immortal
+    /// </summary>
+    [HarmonyPatch(typeof(PartBuoyancy))]
+    [HarmonyPatch("FixedUpdate")]
+    public class PartBuoyancy_FixedUpdate
+    {
+        [HarmonyPrefix]
+        private static bool PrefixCheckPartG(Part ___part)
+        {
+            if (MainSystem.NetworkState < ClientState.Connected || !___part || !___part.vessel) return true;
+
+            if (___part.vessel.IsImmortal())
+                return false;
+
+            //The vessel have updates queued as it was left there by a player in a future subspace
+            if (VesselPositionSystem.Singleton.VesselHavePositionUpdatesQueued(___part.vessel.id))
+                return false;
+
+            return true;
+        }
+    }
+}
