06ddcd7cce1aefc2b8cae1b5bdc586c94db1ff6f
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-12-04 01:16:04 +0100

    Updating nightly back to 0.18.999

--- LmpMasterServer/Properties/AssemblyInfo.cs
+++ LmpMasterServer/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("13549ede-d540-47ba-86bc-731bb7dd7bfb")]
 
-[assembly: AssemblyVersion("0.18.0")]
-[assembly: AssemblyFileVersion("0.18.0")]
-[assembly: AssemblyInformationalVersion("0.18.0-compiled")]
+[assembly: AssemblyVersion("0.18.999")]
+[assembly: AssemblyFileVersion("0.18.999")]
+[assembly: AssemblyInformationalVersion("0.18.999-compiled")]
