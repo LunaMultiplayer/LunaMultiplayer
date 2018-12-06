0af21d975287a2f938863e9bab0d1d54d24cb0af
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-12-06 14:23:05 +0100

    Fix ping calculations

--- LmpClient/VesselUtilities/VesselCommon.cs
+++ LmpClient/VesselUtilities/VesselCommon.cs
@@ -25,8 +25,7 @@ namespace LmpClient.VesselUtilities
     /// </summary>
     public class VesselCommon
     {
-        public static float PositionAndFlightStateMessageOffsetSec(float targetPingSec) =>
-            (int)Mathf.Clamp(NetworkStatistics.PingSec + targetPingSec * 2, 0.250f, 1);
+        public static float PositionAndFlightStateMessageOffsetSec(float targetPingSec) => Mathf.Clamp(NetworkStatistics.PingSec + targetPingSec * 2, 0.250f, 1);
 
         public static bool UpdateIsForOwnVessel(Guid vesselId)
         {
