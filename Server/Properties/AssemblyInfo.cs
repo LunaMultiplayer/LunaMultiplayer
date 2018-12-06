083d69e4dc90d92c5351f01552aa34c1431809c7
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-12-06 21:42:54 +0100

    New nightly

--- Server/Properties/AssemblyInfo.cs
+++ Server/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("fa6e9184-e243-49cc-94fa-ac557493b900")]
 
-[assembly: AssemblyVersion("0.19.111")]
-[assembly: AssemblyFileVersion("0.19.111")]
-[assembly: AssemblyInformationalVersion("0.19.111-compiled")]
+[assembly: AssemblyVersion("0.19.100")]
+[assembly: AssemblyFileVersion("0.19.100")]
+[assembly: AssemblyInformationalVersion("0.19.100-compiled")]
 
 [assembly: InternalsVisibleTo("ServerTest")]
