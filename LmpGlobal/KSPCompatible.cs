0460203ea123c1613a48c4b9562a173ed42d40f6
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2019-01-03 15:15:56 +0100

    Added compatibility with 1.6.0

--- LmpGlobal/KSPCompatible.cs
+++ LmpGlobal/KSPCompatible.cs
@@ -4,7 +4,7 @@ namespace LmpGlobal
 {
     public static class KspCompatible
     {
-        public static readonly Version MinKspVersion = new Version("1.5.1");
-        public static readonly Version MaxKspVersion = new Version("1.5.9");
+        public static readonly Version MinKspVersion = new Version("1.6.0");
+        public static readonly Version MaxKspVersion = new Version("1.6.9");
     }
 }
