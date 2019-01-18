3d70d5ae1188992d39f07cdfef35276b752d1c02
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2019-01-18 15:46:48 +0100

    Display start time in web srv

--- Server/Web/Structures/CurrentState.cs
+++ Server/Web/Structures/CurrentState.cs
@@ -1,5 +1,6 @@
 ﻿using Server.Context;
 using Server.System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,6 +8,7 @@ namespace Server.Web.Structures
 {
     public class CurrentState
     {
+        public DateTime StartTime { get; set; }
         public List<string> CurrentPlayers { get; } = new List<string>();
         public List<VesselInfo> CurrentVessels { get; } = new List<VesselInfo>();
         public List<Subspace> Subspaces { get; } = new List<Subspace>();
@@ -16,6 +18,7 @@ namespace Server.Web.Structures
             CurrentPlayers.Clear();
             CurrentVessels.Clear();
             Subspaces.Clear();
+            StartTime = TimeContext.StartTime;
             CurrentPlayers.AddRange(ServerContext.Clients.Values.Select(v => v.PlayerName));
             CurrentVessels.AddRange(VesselStoreSystem.CurrentVessels.Values.Select(v => new VesselInfo(v)));
             Subspaces.AddRange(WarpContext.Subspaces.Values);
