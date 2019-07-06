e954d437b24a6cce1be582bd50d7e1ec470298d5
gavazquez <gavazquez@gmail.com>
2019-07-06 12:51:44 +0200

    Update to 1.7.2

--- LmpGlobal/KSPCompatible.cs
+++ LmpGlobal/KSPCompatible.cs
@@ -4,7 +4,7 @@ namespace LmpGlobal
 {
     public static class KspCompatible
     {
-        public static readonly Version MinKspVersion = new Version("1.7.1");
+        public static readonly Version MinKspVersion = new Version("1.7.2");
         public static readonly Version MaxKspVersion = new Version("1.7.9");
     }
 }
