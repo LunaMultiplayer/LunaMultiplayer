d53d19fb3e7b21484d07f3ddfa300d0735136802
gavazquez <gavazquez@gmail.com>
2020-05-08 16:31:12 +0200

    Update nightly to 0.26

--- Lidgren.Network/Properties/AssemblyInfo.cs
+++ Lidgren.Network/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("78f2a988-46e1-4fc4-b7b4-dd2cbe46da6a")]
 
-[assembly: AssemblyVersion("0.25.0")]
-[assembly: AssemblyFileVersion("0.25.0")]
-[assembly: AssemblyInformationalVersion("0.25.0-compiled")]
+[assembly: AssemblyVersion("0.26.0")]
+[assembly: AssemblyFileVersion("0.26.0")]
+[assembly: AssemblyInformationalVersion("0.26.0-compiled")]
 
 [assembly: System.CLSCompliant(true)]
 [assembly: InternalsVisibleTo("LmpCommonTest")]
