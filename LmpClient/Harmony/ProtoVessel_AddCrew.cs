1d7fdcf1304f341c93304cbe1bf1f90c65b4ebab
Gabriel Vazquez <gavazquez@gmail.com>
2020-12-28 11:34:39 +0100

    Fix a bug with commnet. Closes #290

---
+++ LmpClient/Harmony/ProtoVessel_AddCrew.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+
+// ReSharper disable All
+
+namespace LmpClient.Harmony
+{
+    /// <summary>
+    /// This harmony patch is intended to fix protovessel add crew. In some cases the pcm comes with a null value since the kerbals are not totally synced and then
+    /// the commnet is messed up and spams the log
+    /// </summary>
+    [HarmonyPatch(typeof(ProtoVessel))]
+    [HarmonyPatch("AddCrew")]
+    public class ProtoVessel_AddCrew
+    {
+        [HarmonyPrefix]
+        private static bool PrefixAddCrew(ProtoCrewMember pcm)
+        {
+            return pcm != null;
+        }
+    }
+}
