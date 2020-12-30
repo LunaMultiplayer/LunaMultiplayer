10d4de5aa3476e05f43b35b4105d62ea88ccf7df
Gabriel Vazquez <gavazquez@gmail.com>
2020-12-30 08:00:20 +0100

    Added a lidgren net and core

---
+++ Lidgren/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lidgren.Network")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lidgren.Network")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("78f2a988-46e1-4fc4-b7b4-dd2cbe46da6a")]
+
+[assembly: AssemblyVersion("0.28.0")]
+[assembly: AssemblyFileVersion("0.28.0")]
+[assembly: AssemblyInformationalVersion("0.28.0-compiled")]
+
+[assembly: System.CLSCompliant(true)]
+[assembly: InternalsVisibleTo("LmpCommonTest")]
