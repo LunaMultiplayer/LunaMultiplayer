77ccaafe0ed5d91c8666c69ec71f069de6a9332a
Gabriel Vazquez <gavazquez@gmail.com>
2021-06-25 21:48:17 +0200

    Do not set paths as appveyor fails

--- Scripts/SetDirectories.bat
+++ Scripts/SetDirectories.bat
@@ -2,6 +2,6 @@
 ::To resume the tracking of changes to this file use the command: git update-index --no-skip-worktree <file>
 
 ::Set the path below as you need and then run this bat to copy the files and make them debuggeable
-SET KSPPATH=F:\Kerbal Space Program
+::SET KSPPATH=F:\Kerbal Space Program
 ::Set another path in case you run two KSP instances otherwise leave empty
-SET KSPPATH2=F:\Kerbal Space Program2
+::SET KSPPATH2=F:\Kerbal Space Program2
