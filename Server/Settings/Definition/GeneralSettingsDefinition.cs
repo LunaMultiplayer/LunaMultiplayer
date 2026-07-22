46a505ee5c111b5546ccecde2cf8ce1608a6c6d4
Shiralyn <68858896+ShiralynDev@users.noreply.github.com>
2026-07-22 21:51:37 +0200

    Update comments var name to match the real var
    Co-authored-by: BraveCaperCat2 <113925022+BraveCaperCat2@users.noreply.github.com>

--- Server/Settings/Definition/GeneralSettingsDefinition.cs
+++ Server/Settings/Definition/GeneralSettingsDefinition.cs
@@ -81,7 +81,7 @@ namespace Server.Settings.Definition
         [XmlComment(Value = "Max number of parts that a vessel can have when spawning")]
         public int MaxVesselParts { get; set; } = 200;
 
-        [XmlComment(Value = "Max amount of backups to keep. 0 = Disabled. BackupInterval in IntervalSettings can be used to change how often to backup, it needs to be above 0 for backups to happen.")]
+        [XmlComment(Value = "Max amount of backups to keep. 0 = Disabled. BackupMinutesInterval in IntervalSettings can be used to change how often to backup, it needs to be above 0 for backups to happen.")]
         public int MaxBackups { get; set; } = 5;
     }
 }
